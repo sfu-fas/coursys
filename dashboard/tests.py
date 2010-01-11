09efd38b7a89b59e88fc012463cde67519037436
Greg Baker <ggbaker@sfu.ca>
2010-01-11 06:53:44 +0000

    work on testing views with fake CAS server

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.test.client import Client
+from settings import CAS_SERVER_URL
 
 from coredata.tests import create_offering
+from coredata.models import CourseOffering
 
 class DashboardTest(TestCase):
     fixtures = ['test_data']
@@ -11,17 +13,28 @@ class DashboardTest(TestCase):
 
     def test_front_page(self):
         client = Client()
-        client.login(username="0kvm", password="")
-        #response = client.get("/")
+        client.login(ticket="0kvm", service=CAS_SERVER_URL)
+
+        response = client.get("/")
+        self.assertEquals(response.status_code, 200)
+        
+        # this student is in this course: check for a link to its page
+        c = CourseOffering.objects.get(slug='1101-cmpt-125-d200')
+        self.assertContains(response, '<a href="%s"' % (c.get_absolute_url()) )
+
         #print response.content
 
 
     def test_course_page(self):
+        """
+        Check out a course front-page
+        """
         s, c = create_offering()
         
         client = Client()
+        client.login(ticket="0kvm", service=CAS_SERVER_URL)
+
         response = client.get(c.get_absolute_url())
-        
         #self.assertEquals(response.status_code, 200)
         
 
