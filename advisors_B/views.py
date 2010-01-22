91a9884673797589112fe690b065acec73de4a86
Jiahui Xu <jiahuix@sfu.ca>
2010-01-22 06:39:18 +0000

    [advisors_B] view.py updated

--- advisors_B/views.py
+++ advisors_B/views.py
@@ -1,21 +1,20 @@
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, loader
 from advisors_B.models import *
 #from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from courselib.auth import requires_advisor
 from django.template import RequestContext
 from coredata.models import Member
-from django.core.urlresolvers import reverse
 from datetime import datetime
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-@login_required
+@requires_advisor()
 def index(request):
     is_advisor = True
-    for advisor in Role.objects.filter(role = 'ADVS'):
+    for advisor in OtherUser.objects.filter(role = 'ADVS'):
         if advisor.person.userid == request.user.username:
             is_advisor = True
             break
@@ -26,25 +25,16 @@ def index(request):
         print note_list
         return render_to_response("advisors_B/student.html",{'note_list':note_list})
 
-@login_required()
+@requires_advisor()
 def search_result(request):
-    return render_to_response("advisors_B/searchresult.html")
+    return render_to_response("advisors_B/search_result.html")
 
 @login_required()
 def create(request, advisor_id, student_id):
     p = Person.objects.get(userid = advisor_id)
-    c_advisor = Role.objects.get(person = p)
-    c_student = Person.objects.get(userid = student_id)
-    new_note=Note(student=c_student,author=c_advisor,create_date = datetime.now())
-    if request.method=='POST':
-        form = NoteForm(request.POST,request.FILES,instance = new_note)
-        if form.is_valid():
-            form.save()
-    		new_note.save()
-            return HttpResponseRedirect(reverse('advisors_B.views.search', args=({'note':new_note},)))   
-    else:
-            form = NoteForm()
-    return render_to_response("advisors_B/create.html", {'advisor':c_advisor, 'student': c_student, 'id':new_note.id})
+    advisor = OtherUser.objects.get(person = p)
+    student = Person.objects.get(userid = student_id)
+    return render_to_response("advisors_B/create.html", {'advisor':advisor, 'student': student})
 
 @login_required
 def detail(request, note_id):
@@ -53,3 +43,18 @@ def detail(request, note_id):
     except Note.DoesNotExist:
         raise Http404
     return render_to_response("advisors_B/detail.html",{'note':n})
+
+@login_required
+def submit(request, advisor_id, student_id):
+	p = get_object_or_404(Person, userid = advisor_id)
+	a = get_object_or_404(OtherUser, person = p)
+	s = get_object_or_404(Person, userid = student_id)
+	con = request.POST['NoteContent']
+	f = request.FILES.get('browns')
+	try:
+		hid = request.POST['hide']
+	except KeyError:
+		hid = False
+	n = Note(content = con, student = s, create_date = datetime.now(), author = a, hidden = hid, file = f)
+	n.save();
+	return render_to_response('advisors_B/succeed.html', {'create_date':datetime.now(), 'advisor_id':advisor_id}) # Redirect after POST
