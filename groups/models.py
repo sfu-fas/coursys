78b7636107333a5435de13f8ca2ad27bc1254b0e
Capstone Student xiong <xiong@sfu.ca>
2010-01-26 09:24:33 +0000

    create basic model

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from coredata.models import Member, CourseOffering
+
 
 # Create your models here.
+class Group(models.Model):
+    """
+    General group information in the courses
+    """
+    name = models.CharField(maxlength=32)
+    manager = models.OneToOneField(Person)
+    course = models.ForeignKey(courseoffering)
+
+    def __unicode__(self):  
+        return self.name
+    
+
+
+class GroupMember(models.Model)
+    """
+    Member information of each group
+    """
+    group=models.ForeignKey(group)
+    student= models.ForeignKey(Member)
+    confirmed= models.BooleanField(default = False)
+    
+    
