116a6914f57825c8a6495d85c156d16021989124
Greg Baker <ggbaker@sfu.ca>
2010-01-26 22:38:06 +0000

    fix XHTML validation code

--- courselib/testing.py
+++ courselib/testing.py
@@ -1,15 +1,21 @@
 import os
 from lxml import etree
 
+
 def validate_content(testcase, data, page_descr="unknown page"):
     """
     Validate data as XHTML 1.0 (strict).
     
     testcase should be a unittest.TestCase object (or similar).
     """
+    # force use of local copy of DTD
+    dtd = os.path.join(os.getcwd(), "courselib/dtd", "xhtml1-strict.dtd")
+    data_system = data.replace("http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd", dtd, 1)
+    assert data != data_system
+
     try:
         parser = etree.XMLParser(dtd_validation=True, no_network=True)
-        etree.fromstring(data, parser=parser)
+        etree.fromstring(data_system, parser=parser)
     except etree.XMLSyntaxError as e:
         #print "-"*40
         #print data
