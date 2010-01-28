0e8f1d861686d964320655a7fddcbaa3edf73f2d
Capstone Student kcc14 <kcc14@sfu.ca>
2010-01-28 11:05:42 +0000

    grades component: added remaining files

---
+++ grades/urls.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from django.conf.urls.defaults import *
+#from courses.urls import COURSE_SLUG_RE
+
+COURSE_SLUG_RE = '\d{4}-[a-z]{2,4}-\w{3,4}-[a-z]\d{3}'
+urlpatterns = patterns('',
+    url(r'^$', 'grades.views.index'),
+    url(r'^(?P<course_slug>' + COURSE_SLUG_RE + ')$', 'grades.views.course'),
+)
