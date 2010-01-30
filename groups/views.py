19706e3f2bc627071343503e703fab9e8a35f11a
Capstone Student lta19 <lta19@sfu.ca>
2010-01-30 06:45:47 +0000

    fix some problems in groups ... please test first before commit

--- groups/views.py
+++ groups/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from coredata.models import Member, Person, CourseOffering
-from group.models import *
+from groups.models import *
 from django.shortcuts import render_to_response, get_object_or_404
 
 @login_required
@@ -17,5 +17,5 @@ def groupmanage(request, course_slug):
 			c = True
 			memberlist = GroupMember.objects.get(group = g)
 		render_to_response('groups/student.html', {'group':g,'confirmed':c,'memberlist':memberlist,'grouplist':grouplist}, context_instance = RequestContext(request))
-	else if m.role == 'INST':
+	elif m.role == 'INST':
 		render_to_response('groups/instructor.html', context_instance = RequestContext(request))
