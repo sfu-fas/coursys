bddef7df288526d6c10506b6ed2b1e5aed9d4b2a
Capstone Student shanw <shanw@sfu.ca>
2010-01-31 06:29:23 +0000

    add views for student’s view

--- grades/views.py
+++ grades/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from coredata.models import Member, CourseOffering
+from coredata.models import Member, CourseOffering,Person
 from courselib.auth import requires_course_by_slug
 from grades.models import ACTIVITY_STATUS
 
@@ -25,3 +25,26 @@ def course(request, course_slug):
     context = {'course': course, 'activities': activities}
     return render_to_response("grades/course.html", context,
                               context_instance=RequestContext(request))
+
+
+
+@login_required
+def student_view(request, student_id):
+    target_student = Person.objects.get(userid = student_id )
+    enrollment= Member.objects.filter(person=target_student).exclude(role="DROP")
+    context ={'student_id':student_id,'enrollment':enrollment,'student':target_student}
+    #student's view:
+    if target_student.userid == request.user.username:      
+        return render_to_response('grades/student_view.html', context,
+                                  context_instance=RequestContext(request))
+
+
+@requires_course_by_slug
+def student_grade(request,student_id,course_slug):
+    course = CourseOffering.objects.get(slug=course_slug)
+    activities = course.activity_set.all()
+    
+    context = {'student_id':student_id, 'course': course, 'activities': activities}
+    return render_to_response("grades/student_grade.html", context,
+                              context_instance=RequestContext(request))
+
