782d9a157758d2fee8f79c55ac5cf3342764413a
Capstone Student shanw <shanw@sfu.ca>
2010-01-31 06:28:34 +0000

    add url for student's view

--- grades/urls.py
+++ grades/urls.py
@@ -7,4 +7,6 @@ COURSE_SLUG_RE = '\d{4}-[a-z]{2,4}-\w{3,4}-[a-z]\d{3}'
 urlpatterns = patterns('',
     url(r'^$', 'grades.views.index'),
     url(r'^(?P<course_slug>' + COURSE_SLUG_RE + ')$', 'grades.views.course'),
+    url(r'^(?P<student_id>\w+)$', 'grades.views.student_view'),
+    url(r'^(?P<student_id>\w+)/'+'(?P<course_slug>'+ COURSE_SLUG_RE + ')$', 'grades.views.student_grade'),
 )
