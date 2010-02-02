25624cd4d9ff95b178dcd8a09150d47b51ee5222
Jiahui Xu <jiahuix@sfu.ca>
2010-02-02 20:13:09 +0000

    [group] view.py updated

--- groups/urls.py
+++ groups/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 COURSE_SLUG_RE = '\d{4}-[a-z]{2,4}-\w{3,4}-[a-z]\d{3}'
 urlpatterns = patterns('',
-	#url(r'^$', 'groups.views.index'),
+	url(r'^$', 'groups.views.index'),
 	url(r'^(?P<course_slug>' + COURSE_SLUG_RE + ')/$', 'groups.views.groupmanage'),
 )
