b777dea36ba593f30f95eeb89df42dff46c2160c
Jiahui Xu <jiahuix@sfu.ca>
2010-02-05 08:22:12 +0000

    [groups] urls.py updated

--- groups/urls.py
+++ groups/urls.py
@@ -4,5 +4,7 @@ COURSE_SLUG_RE = '\d{4}-[a-z]{2,4}-\w{3,4}-[a-z]\d{3}'
 urlpatterns = patterns('',
 	url(r'^$', 'groups.views.index'),
 	url(r'^(?P<course_slug>' + COURSE_SLUG_RE + ')/$', 'groups.views.groupmanage'),
-                       url(r'^create/$', 'groups.views.create'),
+	url(r'^create/$', 'groups.views.create'),
+	url(r'^(?P<course_slug>' + COURSE_SLUG_RE + ')/join/(?P<groupname>\w+)/$', 'groups.views.join'),
+	url(r'^(?P<course_slug>' + COURSE_SLUG_RE + ')/joinconfirm/$', 'groups.views.joinconfirm'),
 )
