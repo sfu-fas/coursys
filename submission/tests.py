14d30484449c75c980c856809f1c03709f1f1258
Greg Baker <ggbaker@sfu.ca>
2010-02-09 19:59:58 +0000

    tests for submission component selection

--- submission/tests.py
+++ submission/tests.py
@@ -1,23 +1,33 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from submission.models import *
+from grades.models import NumericActivity
+from coredata.tests import create_offering
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class SubmissionTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_components(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test submission component classes: subclasses, selection, sorting.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        s, c = create_offering()
+        a = NumericActivity(name="Assignment 1", short_name="A1", status="RLS", offering=c, position=2, max_grade=15)
+        a.save()
+        
+        c = URLComponent(activity=a, title="URL", position=8)
+        c.save()
+        c = ArchiveComponent(activity=a, title="Archive", position=1, max_size=100000)
+        c.save()
+        c = PlainTextComponent(activity=a, title="Text", position=89)
+        c.save()
+        c = CppComponent(activity=a, title="CPP", position=2)
+        c.save()
+        
+        comps = AllComponents(a)
+        self.assertEqual(len(comps), 4)
+        self.assertEqual(comps[0].title, 'Archive') # make sure position=1 is first
+        self.assertEqual(type(comps[1]), CppComponent)
+        self.assertEqual(type(comps[3]), PlainTextComponent)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
 
