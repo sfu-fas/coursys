18b5faba794a545dd87526068616586bb9f12c12
Capstone Student shanw <shanw@sfu.ca>
2010-03-16 16:08:23 +0000

    modifiy views for news item

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,13 +4,17 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from coredata.models import Member, CourseOffering
 from courselib.auth import requires_course_by_slug
+from dashboard.models import NewsItem
 
 @login_required
 def index(request):
     userid = request.user.username
     memberships = Member.objects.exclude(role="DROP").filter(offering__graded=True).filter(person__userid=userid) \
             .select_related('offering','person','offering__semester')
-    return render_to_response("dashboard/index.html", {'memberships': memberships}, context_instance=RequestContext(request))
+    news_list = NewsItem.objects.filter(user=userid).order_by('updated')[:5]
+
+    context = {'memberships': memberships ,'news_list': news_list}
+    return render_to_response("dashboard/index.html",context ,context_instance=RequestContext(request))
 
 #@requires_course_by_slug
 #def course(request, course_slug):
