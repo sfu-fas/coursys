61d2035e2a10d3fdffd97d0bab32a3c0146f5bef
Capstone Student xiong <xiong@sfu.ca>
2010-03-23 09:36:18 +0000

    make modification on the activty form to make the field "readonly"

--- groups/forms.py
+++ groups/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django import forms
 from grades.models import Activity
 from coredata.models import Person
+from django.forms.util import flatatt
 
 class GroupForm(forms.Form):
     name=forms.CharField(max_length=30, label='Name:')
@@ -13,10 +14,16 @@ class GroupForm(forms.Form):
 
 class ActivityForm(ModelForm):
     selected = forms.BooleanField(label = 'Selected Activity:', required = False)
-    
+    def __init__(self, *args, **kwargs):
+        super(ActivityForm, self).__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance and instance.id:
+            self.fields.widget.attrs['readonly'] = True
+            
     class Meta:
         model = Activity
         fields = ["selected","name","percent","due_date"]
+
         
 class StudentForm(forms.Form):
     selected = forms.BooleanField(label = 'Selected Student:', required = False)
