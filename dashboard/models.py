ecd355c8df381707dc35c6f15e60ac427c8d62ec
Capstone Student shanw <shanw@sfu.ca>
2010-03-23 06:34:04 +0000

    modelform for manual-created NewsItem

--- dashboard/models.py
+++ dashboard/models.py
@@ -13,13 +13,20 @@ class NewsItem(models.Model):
     author = models.ForeignKey(Person, null=True, related_name="author")
     course = models.ForeignKey(CourseOffering, null=True)
     source_app = models.CharField(max_length=20, null=False, help_text="Application that created the story")
-    
+
     title = models.CharField(max_length=100, null=False, help_text="Story title (plain text)")
     content = models.TextField(help_text="Main story content (Textile markup)")
     published = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    url = models.URLField(verify_exists=False, help_text='absolute URL for the item: starts with "http://" or "/"')
-    
+    url = models.URLField(blank=True,verify_exists=False, help_text='absolute URL for the item: starts with "http://" or "/"')
+
     #def content_xhtml(self):
     #    return Textile.textile(str(self.content))
+
+from django.forms import ModelForm
+class MessageForm(ModelForm):
+    class Meta:
+        model = NewsItem
+        # these 3 fields are decided from the request ant the time the form is submitted
+        exclude = ['user', 'author', 'published','updated','source_app','course']
 
