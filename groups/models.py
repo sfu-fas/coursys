8358b5c9cb46f18356d372ffa31e616ebbe2bff0
Capstone Student shanw <shanw@sfu.ca>
2010-04-09 20:07:53 +0000

    NewsItem for group invitation

--- groups/models.py
+++ groups/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from coredata.models import Member, CourseOffering
 from autoslug import AutoSlugField
 from grades.models import Activity
+from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse
+from dashboard.models import NewsItem
 
 class Group(models.Model):
     """
@@ -10,13 +13,13 @@ class Group(models.Model):
     name = models.CharField(max_length=30, help_text='Group name')
     manager = models.ForeignKey(Member, blank=False, null=False)
     courseoffering = models.ForeignKey(CourseOffering)
-    
+
     # preface slug with "g-" to avoid conflict with userids (so they can be used in same places in URLs)
     def autoslug(self):
         return 'g-' + str(self.name)
     slug = AutoSlugField(populate_from=autoslug, null=False, editable=False, unique_with='courseoffering')
 
-    def __unicode__(self):  
+    def __unicode__(self):
         return '%s' % (self.name)
     def delete(self, *args, **kwargs):
         raise NotImplementedError, "This object cannot be deleted because it is used as a foreign key."
@@ -31,21 +34,22 @@ class GroupMember(models.Model):
     group = models.ForeignKey(Group)
     student = models.ForeignKey(Member)
     confirmed = models.BooleanField(default = False)
-    activity = models.ForeignKey(Activity) 
+    activity = models.ForeignKey(Activity)
 
     def __unicode__(self):
 	    return '%s@%s' % (self.student.person, self.group)
 
-    #def save(self):
-     #   super(GroupMember, self).save()
-      #  member = get_object_or_404(Member, person = person)
-       # n = NewsItem(user = student.person, author=Null, course=member.offering,
-        #    source_app="Group Join", title="Group Conformation", 
-         #   content="You have joined in group %s."
-          #      % (group),
-           # url=reverse('group.views.groupmanage', kwargs={'course_slug':member.offering.slug})
-            #)
-        #n.save()
-	
+    def save(self,person):
+        super(GroupMember, self).save()
+        if self.confirmed == False:
+            course = get_object_or_404(CourseOffering, slug = self.group.courseoffering.slug)
+            member = get_object_or_404(Member, person = self.student.person,offering=course)
+            n = NewsItem(user = person,author = self.student.person, course=member.offering,
+                         source_app="Group Join", title="Group Conformation",
+                         content="You have been invited to join group %s." % (self.group),
+                         url=reverse('groups.views.groupmanage', kwargs={'course_slug':course.slug})
+            )
+            n.save()
+
     class Meta:
         unique_together = ("group", "student", "activity")
