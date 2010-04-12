2d6dc46d76ce87ccc0bda91929bd976dd5e93244
Greg Baker <ggbaker@sfu.ca>
2010-04-12 06:10:37 +0000

    more group tests

--- groups/tests.py
+++ groups/tests.py
@@ -8,6 +8,7 @@ from grades.models import *
 from courselib.testing import *
 from groups.models import *
 from django.core.urlresolvers import reverse
+from django.db import IntegrityError
 
 import re
 
@@ -17,6 +18,71 @@ class GroupTest(TestCase):
     def setUp(self):
         pass
 
+    def test_group_models(self):
+        """
+        Test the backend for groups
+        """
+        s, c = create_offering()
+        a = NumericActivity(name="Assignment 1", short_name="A1", status="URLS", offering=c, position=3, max_grade=20, group=True)
+        a.save()
+        a1 = a
+        a = NumericActivity(name="Assignment 2", short_name="A2", status="URLS", offering=c, position=6, max_grade=20, group=True)
+        a.save()
+        a2 = a
+        
+        userid1 = "0kvm"
+        userid2 = "0aaa0"
+        userid3 = "0aaa1"
+        userid4 = "0aaa2"
+        for u in [userid1, userid2, userid3, userid4]:
+            p = Person.objects.get(userid=u)
+            m = Member(person=p, offering=c, role="STUD", credits=3, career="UGRD", added_reason="UNK")
+            m.save()
+        
+        # basics
+        m = Member.objects.get(person__userid=userid1, offering=c)
+        g = Group(name="Test Group", manager=m, courseoffering=c)
+        g.save()
+        self.assertEqual(g.slug, 'g-test-group')
+        gm = GroupMember(group=g, student=m, confirmed=True, activity=a1)
+        gm.save()
+        m = Member.objects.get(person__userid=userid2, offering=c)
+        gm = GroupMember(group=g, student=m, confirmed=True, activity=a1)
+        gm.save()
+        
+        gs = Group.objects.filter(courseoffering=c)
+        self.assertEqual(len(gs), 1)
+        g = gs[0]
+        self.assertEqual(set([gm.student.person.userid for gm in g.groupmember_set.all()]), set([userid1,userid2]))
+
+        # check uniqueness of activity + member        
+        m = Member.objects.get(person__userid=userid3, offering=c)
+        g2 = Group(name="Other Group", manager=m, courseoffering=c)
+        g2.save()
+        gm = GroupMember(group=g2, student=m, confirmed=True, activity=a1)
+        gm.save()
+        
+        gm = GroupMember(group=g, student=m, confirmed=True, activity=a1)
+        self.assertRaises(IntegrityError, gm.save)
+        
+        # uniqueness of group name
+        g3 = Group(name="Other Group", manager=m, courseoffering=c)
+        self.assertRaises(IntegrityError, g3.save)
+        
+        # finding all activities this group covers
+        members = GroupMember.objects.filter(group=g)
+        all_act = all_activities(members)
+        self.assertEqual(set(a.slug for a in all_act), set([a1.slug]))
+
+        # add a member for assignment 2 and check again
+        gm = GroupMember(group=g, student=m, confirmed=True, activity=a2)
+        gm.save()
+        members = GroupMember.objects.filter(group=g)
+        all_act = all_activities(members)
+        self.assertEqual(set(a.slug for a in all_act), set([a1.slug, a2.slug]))
+
+
+
     def test_group_student(self):
         """
         Check out group pages for student: go through the whole group-creation process from the student side.
@@ -24,13 +90,15 @@ class GroupTest(TestCase):
         s, c = create_offering()
         a = NumericActivity(name="Assignment 1", short_name="A1", status="URLS", offering=c, position=3, max_grade=20, group=True)
         a.save()
+        a1 = a
         a = NumericActivity(name="Assignment 2", short_name="A2", status="URLS", offering=c, position=6, max_grade=20, group=True)
         a.save()
         
         userid1 = "0kvm"
         userid2 = "0aaa0"
         userid3 = "0aaa1"
-        for u in [userid1, userid2, userid3]:
+        userid4 = "0aaa2"
+        for u in [userid1, userid2, userid3, userid4]:
             p = Person.objects.get(userid=u)
             m = Member(person=p, offering=c, role="STUD", credits=3, career="UGRD", added_reason="UNK")
             m.save()
@@ -56,6 +124,7 @@ class GroupTest(TestCase):
         gs =  Group.objects.filter(courseoffering=c)
         self.assertEquals(len(gs), 1)
         self.assertEquals(gs[0].name, "Test Group")
+        self.assertEquals(gs[0].manager.person.userid, userid1)
 
         gms = GroupMember.objects.filter(group__courseoffering=c)
         self.assertEquals(len(gms), 1)
@@ -98,6 +167,21 @@ class GroupTest(TestCase):
         
         gms = GroupMember.objects.filter(group__courseoffering=c, student__person__userid=userid3)
         self.assertEquals(len(gms), 0)
+
+        # inviting userid4 shouldn't work if already in a group
+        m = Member.objects.get(person__userid=userid4, offering=c)
+        g = Group(name="Other Group", manager=m, courseoffering=c)
+        g.save()
+        gm = GroupMember(group=g, student=m, confirmed=True, activity=a1)
+        gm.save()
+        
+        client.login(ticket=userid1, service=CAS_SERVER_URL)
+        url = reverse('groups.views.invite', kwargs={'course_slug': c.slug, 'group_slug':'g-test-group'})
+        response = client.post(url, {"name": userid4})
+        
+        gms = GroupMember.objects.filter(group__courseoffering=c, student__person__userid=userid4)
+        self.assertEqual(len(gms), 1)
+        self.assertEqual(gms[0].group.slug, 'g-other-group')
 
 
     def test_group_staff(self):
