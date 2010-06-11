ffc1b4cfdb30d6cd5e0727b9e1da94ed6e6639b0
Greg Baker <ggbaker@sfu.ca>
2010-06-11 01:37:23 +0000

    update group admin interface

--- groups/admin.py
+++ groups/admin.py
@@ -1,5 +1,24 @@
 from courses.groups.models import *
 from django.contrib import admin
 
-admin.site.register(Group)
-admin.site.register(GroupMember)
+
+class GMemberInline(admin.TabularInline):
+    model = GroupMember
+    fk_name = "group"
+    #extra = 2
+
+class GAdmin(admin.ModelAdmin):
+    list_display = ('name', 'courseoffering')
+    search_fields = ('name', 'courseoffering')
+    #list_filter = ('name', 'courseoffering')
+    inlines = [GMemberInline]
+
+class GMAdmin(admin.ModelAdmin):
+    list_display = ('activity', 'group', 'student')
+    search_fields = ('activity', 'group', 'student')
+    #list_filter = ('activity', 'group')
+    
+
+
+admin.site.register(Group, GAdmin)
+admin.site.register(GroupMember, GMAdmin)
