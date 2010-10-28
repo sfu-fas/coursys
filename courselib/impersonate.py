be5a3654e66781a9fa83ed3202a3536a70175b53
Greg Baker <ggbaker@sfu.ca>
2010-10-28 20:25:58 +0000

    allow instructors to impersonate within their courses

--- courselib/impersonate.py
+++ courselib/impersonate.py
@@ -5,10 +5,33 @@ from django.contrib.auth.models import User
 from courselib.auth import has_role
 from log.models import LogEntry
 
+from coredata.models import Member
+from urlparts import COURSE_SLUG
+import re
+
+course_path_re = re.compile("^/" + COURSE_SLUG + "/")
+
 class ImpersonateMiddleware(object):
     def process_request(self, request):
-        if "__impersonate" in request.GET and has_role('SYSA', request.user):
+        if "__impersonate" in request.GET:
+            # impersonation requested: check if it's allowed
             userid = request.GET["__impersonate"]
+            match = course_path_re.match(request.path)
+            if has_role('SYSA', request.user):
+                # for sysadmins: yes.
+                pass
+            elif match:
+                # for instructors of a course: yes, but only within that course's "directory".
+                course_slug = match.group('course_slug') # course slug from the URL
+                memberships = Member.objects.filter(person__userid=request.user.username, offering__slug=course_slug, role="INST")
+                if not memberships:
+                    # this person is not an instructor for this course: no
+                    return
+            else:
+                # no for anybody else: just ignore the impersonation request.
+                return
+
+            # handle the impersonation
             try:
                 user = User.objects.get(username__iexact=userid)
             except User.DoesNotExist:
