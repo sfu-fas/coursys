e7dd9a19f7363347ebc32b0dc2d637f2d2365271
Greg Baker <ggbaker@sfu.ca>
2011-01-11 18:09:09 +0000

    add advisors app for orientation sprint

--- settings.py
+++ settings.py
@@ -120,10 +120,7 @@ INSTALLED_APPS = (
     'groups',
     'submission',
     'planning',
-
-    # for orientation project only
-    #'advisors_A',
-    #'advisors_B',
+    'advisors',
 )
 if DEBUG:
     #INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
