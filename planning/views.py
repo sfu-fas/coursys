f0aae00ded0a29568a09b66c214e07c144c0435f
Capstone Student sga52 <sga52@sfu.ca>
2011-02-21 21:20:21 +0000

    planning module changing UI

--- planning/views.py
+++ planning/views.py
@@ -20,37 +20,45 @@ def courses(request):
 	userid = request.user.username
 	instructor = get_object_or_404(Person, userid = request.user.username)
 	plan_list = TeachingCapability.objects.filter(instructor = instructor).order_by('course')
+	intention_list = TeachingIntention.objects.filter(instructor = instructor).order_by('semester')
  		
-	return render_to_response("planning/index.html",{'userid':userid, 'plan_list':plan_list},context_instance=RequestContext(request))
+	return render_to_response("planning/index.html",{'userid':userid, 'plan_list':plan_list, 'intention_list':intention_list},context_instance=RequestContext(request))
 	
 
 @login_required
 def add_plan(request, userid):
-    
-	return render_to_response("planning/add_plan.html",{'userid':userid},context_instance=RequestContext(request))
+   	
+	course_list = Course.objects.filter()
+	
+	return render_to_response("planning/add_plan.html",{'userid':userid, 'course_list':course_list},context_instance=RequestContext(request))
 
 
-@login_required
-def edit_plan(request, userid):
-
-	return render_to_response("planning/edit_plan.html",{'userid':userid},context_instance=RequestContext(request))
-		
 @login_required
 def submit_plan(request, userid):
     
+	course_number = request.POST['offering_courses']
+	#semester = request.POST['semester']
+	course_count = request.POST['course_count']
+	
 	instructor = get_object_or_404(Person, userid = request.user.username)
-	course = get_object_or_404(Course, title = "D300")
-#	semester = get_object_or_404(Semester, name = '1097')
+	course = get_object_or_404(Course, number = course_number)
+	semester = get_object_or_404(Semester, name = '1097')
+	
 	
 	added_plan = TeachingCapability(instructor = instructor, course = course)
-	added_plan.save();
-
-	messages.add_message(request, messages.SUCCESS, 'Submit Successfully.')
-	return HttpResponseRedirect(reverse(add_plan, kwargs={'userid':userid}))
+	added_plan.save()
+	intention = TeachingIntention(instructor = instructor, semester = semester, count = course_count)
+	intention.save()
 	
 
+	messages.add_message(request, messages.SUCCESS, 'Plan Submitted Successfully.')
+	return HttpResponseRedirect(reverse(add_plan, kwargs={'userid':userid}))
+	#return HttpResponse(course_number)
+	
+	
 @login_required
 def add_course(request, userid):
+#    form = PlanForm()
 
 	return render_to_response("planning/add_courses.html",{'userid':userid},context_instance=RequestContext(request))
 
