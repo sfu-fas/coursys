2e3f9e0836e5e897442c0073d6242db52443220b
Greg Baker <ggbaker@sfu.ca>
2011-02-22 20:39:32 +0000

    script to seed the planning module with initial data

---
+++ planning/setup.py
@@ -0,0 +1,35 @@
+# creates initial data for planning module, based on historical offerings
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from coredata.models import *
+from planning.models import *
+
+# find all courses in the system from previous semesters
+courses = {}
+taught = {}
+for o in CourseOffering.objects.filter(graded=True):
+    key = (o.subject, o.number)
+    courses[key] = o.title
+    if key not in taught:
+        taught[key] = set()
+    for i in o.member_set.filter(role="INST"):
+        taught[key].add(i.person)
+
+for subj, num in courses:
+    # create Course object
+    title = courses[(subj, num)]
+    cs = Course.objects.filter(subject=subj, number=num)
+    if not cs:
+        c = Course(subject=subj, number=num, title=title)
+        c.save()
+    else:
+        c = cs[0]
+
+    # add teaching capabilities for previous instructors
+    for p in taught[(subj, num)]:
+        if not TeachingCapability.objects.filter(course=c, instructor=p):
+            t = TeachingCapability(course=c, instructor=p)
+            t.save()
+
