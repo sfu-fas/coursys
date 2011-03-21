0150652ff55c0460cbb85b0785542ad9e149771e
Capstone Student youyouy <youyouy@sfu.ca>
2011-03-21 18:04:22 +0000

    Added page to display the search_student result for staff on mobile website.

--- mobile/views.py
+++ mobile/views.py
@@ -1,3 +1,4 @@
+import shlex
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -104,6 +105,8 @@ def activity_info(request, course_slug, activity_slug):
 def _activity_info_student(request, course_slug, activity_slug):
     return HttpResponse('Student View')
 
+@login_required
+@gzip_page
 def _activity_info_staff(request, course_slug, activity_slug):
     """
     activity detail page
@@ -119,17 +122,50 @@ def _activity_info_staff(request, course_slug, activity_slug):
     return render_to_response("mobile/activity_info_staff.html", context,
                             context_instance=RequestContext(request))
 
+@login_required
+@gzip_page
+def student_activity_search(request, course_slug, activity_slug):
+    course = get_object_or_404(CourseOffering, slug=course_slug)
+    activities = all_activities_filter(slug=activity_slug, offering=course)
+    if len(activities) != 1:
+        return NotFoundResponse(request)
 
-def student_activity_info(request, course_slug, activity_slug):
-#    course = get_object_or_404(CourseOffering, slug=course_slug)
-#    activities = all_activities_filter(slug=activity_slug, offering=course)
-#    if len(activities) != 1:
-#        return NotFoundResponse(request)
-#
-#    activity = activities[0]
-#    student_info = request.GET.get('q')
-#
-#    student =
+    activity = activities[0]
+
+    q = request.GET.get('q')
+    q = q.encode('ascii','ignore')
+    print q
+    try:
+        keywords = shlex.split(q)
+    except:
+        return HttpResponse("Please check your input.")
+    if keywords == None :
+        context = {'course': course, 'activity': activity}
+    else:
+        students = Member.objects.filter(role="STUD", offering=activity.offering).select_related('person')
+        for kw in keywords:
+            new_list = []
+            for s in students:
+                ss = s.person.last_name+\
+                            s.person.middle_name+\
+                            s.person.first_name+\
+                            s.person.userid+\
+                            str(s.person.emplid)
+                if kw in ss:
+                    new_list.append(s)
+            students = new_list[:]
 
 
-    return HttpResponse("student search")
+    context = {'course': course, 'activity': activity, 'student_list':students}
+    return render_to_response("mobile/search_student.html", context,
+                            context_instance=RequestContext(request))
+
+def student_activity_info(request, course_slug, activity_slug, userid):
+    course = get_object_or_404(CourseOffering, slug=course_slug)
+    activities = all_activities_filter(slug=activity_slug, offering=course)
+    if len(activities) != 1:
+        return NotFoundResponse(request)
+    activity = activities[0]
+    student = Member.objects.get(offering=course, person__userid=userid, role='STUD')
+    return HttpResponse(student.person.last_name)
+    
