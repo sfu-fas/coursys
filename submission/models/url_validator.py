ff14acbc2fac650aca2cc88332da46c6377912e5
Greg Baker <ggbaker@sfu.ca>
2011-05-28 16:41:25 +0000

    fix missing import

--- submission/models/url_validator.py
+++ submission/models/url_validator.py
@@ -1,5 +1,7 @@
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+from django.utils.encoding import smart_unicode
+import urlparse
 
 class QuickURLValidator(URLValidator):
     """
