c78f6c79bb9afee600e9e351f3f57480659a122d
Greg Baker <ggbaker@sfu.ca>
2011-06-02 15:59:54 +0000

    better all instructor script

--- tools/allinstr.py
+++ tools/allinstr.py
@@ -6,4 +6,4 @@ from coredata.models import *
 
 courses = CourseOffering.objects.filter(semester__name="1114")
 instr = itertools.chain(*(c.member_set.filter(role="INST") for c in courses if c.activity_set.count()>0))
-print set([i.person.userid for i in instr])
+print ", ".join(set([i.person.email() for i in instr]))
