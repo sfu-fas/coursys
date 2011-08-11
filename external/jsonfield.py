2d221892338d413dde2649d8192572e92abf836e
Greg Baker <ggbaker@sfu.ca>
2011-08-11 15:46:43 +0000

    update external fields to squash some Django 1.3 warnings

--- external/jsonfield.py
+++ external/jsonfield.py
@@ -39,7 +39,7 @@ class JSONField(models.TextField):
 
         return value
 
-    def get_db_prep_save(self, value):
+    def get_db_prep_save(self, value, connection=None):
         """Convert our JSON object to a string before we save"""
 
         if value == "":
@@ -48,7 +48,7 @@ class JSONField(models.TextField):
         if isinstance(value, dict):
             value = json.dumps(value, cls=DjangoJSONEncoder)
 
-        return super(JSONField, self).get_db_prep_save(value)
+        return super(JSONField, self).get_db_prep_save(value, connection=connection)
 
 
 # from http://south.aeracode.org/wiki/MyFieldsDontWork
--- external/timezones/fields.py
+++ external/timezones/fields.py
@@ -1,51 +1,64 @@
-
-from django.db import models
 from django.conf import settings
-from django.utils.encoding import smart_unicode, smart_str
+from django.db import models
 from django.db.models import signals
-
-from timezones import forms
+from django.utils.encoding import smart_unicode, smart_str
 
 import pytz
+
+from timezones import forms, zones
+from timezones.utils import coerce_timezone_value, validate_timezone_max_length
+
+
 
 MAX_TIMEZONE_LENGTH = getattr(settings, "MAX_TIMEZONE_LENGTH", 100)
 default_tz = pytz.timezone(getattr(settings, "TIME_ZONE", "UTC"))
 
 
-assert reduce(lambda x, y: x and (len(y) <= MAX_TIMEZONE_LENGTH),
-    forms.ALL_TIMEZONE_CHOICES, True), \
-    "timezones.fields.TimeZoneField MAX_TIMEZONE_LENGTH is too small"
-
 class TimeZoneField(models.CharField):
+    
+    __metaclass__ = models.SubfieldBase
+    
     def __init__(self, *args, **kwargs):
-        defaults = {"max_length": MAX_TIMEZONE_LENGTH,
-                    "default": settings.TIME_ZONE,
-                    "choices": forms.PRETTY_TIMEZONE_CHOICES}
+        validate_timezone_max_length(MAX_TIMEZONE_LENGTH, zones.ALL_TIMEZONE_CHOICES)
+        defaults = {
+            "max_length": MAX_TIMEZONE_LENGTH,
+            "default": settings.TIME_ZONE,
+            "choices": zones.PRETTY_TIMEZONE_CHOICES
+        }
         defaults.update(kwargs)
         return super(TimeZoneField, self).__init__(*args, **defaults)
-        
+    
+    def validate(self, value, model_instance):
+        # coerce value back to a string to validate correctly
+        return super(TimeZoneField, self).validate(smart_str(value), model_instance)
+    
+    def run_validators(self, value):
+        # coerce value back to a string to validate correctly
+        return super(TimeZoneField, self).run_validators(smart_str(value))
+    
     def to_python(self, value):
         value = super(TimeZoneField, self).to_python(value)
         if value is None:
             return None # null=True
-        return pytz.timezone(value)
-        
-    def get_db_prep_save(self, value):
-        # Casts timezone into string format for entry into database.
+        return coerce_timezone_value(value)
+    
+    def get_prep_value(self, value):
         if value is not None:
-            value = smart_unicode(value)
-        return super(TimeZoneField, self).get_db_prep_save(value)
-
+            return smart_unicode(value)
+        return value
+    
+    def get_db_prep_save(self, value, connection=None):
+        """
+        Prepares the given value for insertion into the database.
+        """
+        return self.get_prep_value(value)
+    
     def flatten_data(self, follow, obj=None):
         value = self._get_val_from_obj(obj)
         if value is None:
             value = ""
         return {self.attname: smart_unicode(value)}
 
-    def formfield(self, **kwargs):
-        defaults = {"form_class": forms.TimeZoneField}
-        defaults.update(kwargs)
-        return super(TimeZoneField, self).formfield(**defaults)
 
 class LocalizedDateTimeField(models.DateTimeField):
     """
@@ -62,32 +75,37 @@ class LocalizedDateTimeField(models.DateTimeField):
         else:
             self.timezone = timezone
         super(LocalizedDateTimeField, self).__init__(verbose_name, name, **kwargs)
-        
+    
     def formfield(self, **kwargs):
         defaults = {"form_class": forms.LocalizedDateTimeField}
         if (not isinstance(self.timezone, basestring) and str(self.timezone) in pytz.all_timezones_set):
             defaults["timezone"] = str(self.timezone)
         defaults.update(kwargs)
         return super(LocalizedDateTimeField, self).formfield(**defaults)
-        
-    def get_db_prep_save(self, value):
-        "Returns field's value prepared for saving into a database."
+    
+    def get_db_prep_save(self, value, connection=None):
+        """
+        Returns field's value prepared for saving into a database.
+        """
         ## convert to settings.TIME_ZONE
         if value is not None:
             if value.tzinfo is None:
                 value = default_tz.localize(value)
             else:
                 value = value.astimezone(default_tz)
-        return super(LocalizedDateTimeField, self).get_db_prep_save(value)
-        
-    def get_db_prep_lookup(self, lookup_type, value):
-        "Returns field's value prepared for database lookup."
+        return super(LocalizedDateTimeField, self).get_db_prep_save(value, connection=connection)
+    
+    def get_db_prep_lookup(self, lookup_type, value, connection=None, prepared=None):
+        """
+        Returns field's value prepared for database lookup.
+        """
         ## convert to settings.TIME_ZONE
         if value.tzinfo is None:
             value = default_tz.localize(value)
         else:
             value = value.astimezone(default_tz)
-        return super(LocalizedDateTimeField, self).get_db_prep_lookup(lookup_type, value)
+        return super(LocalizedDateTimeField, self).get_db_prep_lookup(lookup_type, value, connection=connection, prepared=prepared)
+
 
 def prep_localized_datetime(sender, **kwargs):
     for field in sender._meta.fields:
@@ -138,8 +156,17 @@ def prep_localized_datetime(sender, **kwargs):
 ##           time_zone value is a valid query keyword (if it is one)
 signals.class_prepared.connect(prep_localized_datetime)
 
+
+# allow South to handle TimeZoneField smoothly
 try:
     from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^timezones\.fields\.TimeZoneField"])
+    add_introspection_rules(rules=[(
+                                    (TimeZoneField, ), # Class(es) these apply to
+                                    [], # Positional arguments (not used)
+                                    { # Keyword argument
+                                        "max_length": ["max_length", { "default": MAX_TIMEZONE_LENGTH }],
+                                    }
+                                )],
+                                patterns=['timezones\.fields\.'])
 except ImportError:
-    pass # ignore missing south module.
+    pass
