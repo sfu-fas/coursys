1c438981d4103656cce0f1036ee8eda4f4de73b0
Greg Baker <ggbaker@sfu.ca>
2011-08-18 06:47:51 +0000

    revert last change, which didn't fix it.

--- external/django_cas/backends.py
+++ external/django_cas/backends.py
@@ -69,11 +69,11 @@ class CASBackend(object):
     def authenticate(self, ticket, service):
         """Verifies CAS ticket and gets or creates User object"""
 
-        username = _verify(ticket, service).lower()
+        username = _verify(ticket, service)
         if not username:
             return None
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(username__iexact=username)
         except User.DoesNotExist:
             # user will have an "unusable" password
             user = User.objects.create_user(username, '')
