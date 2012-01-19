16d74628195b30f398219c94d55bf58a46d73e91
Capstone Student dcc4 <dcc4@sfu.ca>
2012-01-19 03:39:01 +0000

    List all TUGs implemented

--- ta/views.py
+++ ta/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, render
 from courselib.auth import requires_course_staff_by_slug, is_course_staff_by_slug, is_course_student_by_slug, ForbiddenResponse, NotFoundResponse
-from coredata.models import CourseOffering
+from ta.models import TUG
+from coredata.models import *
 
 @requires_course_staff_by_slug
 def index_page(request, course_slug):
@@ -12,7 +13,11 @@ def index_page(request, course_slug):
         
 @requires_course_staff_by_slug
 def all_tugs(request, course_slug):
-    return HttpResponse('All TUGs page')
+    course = get_object_or_404(CourseOffering, slug=course_slug)
+    tas = Member.objects.filter(offering=course, role="TA")
+    
+    context = {'tas': tas, 'course': course.name, 'course_slug': course_slug}
+    return render(request, 'ta/all_tugs.html', context)
 
 @requires_course_staff_by_slug    
 def new_tug(request, course_slug):
@@ -21,7 +26,7 @@ def new_tug(request, course_slug):
         return HttpResponseRedirect(reverse('ta.views.all_tugs', kwargs={}))
     
     else:
-        return render_to_response('ta/new_tug.html',{'course':course})
+        return render(request,'ta/new_tug.html',{'course':course})
 
 @requires_course_staff_by_slug    
 def view_tug(request, course_slug, userid):
