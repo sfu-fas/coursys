656ff25a3e40e0e79633f15322a316a49929c5f4
Capstone Student dcc4 <dcc4@sfu.ca>
2012-01-19 05:18:12 +0000

    View TUG implemented

--- ta/views.py
+++ ta/views.py
@@ -16,7 +16,10 @@ def all_tugs(request, course_slug):
     course = get_object_or_404(CourseOffering, slug=course_slug)
     tas = Member.objects.filter(offering=course, role="TA")
     
-    context = {'tas': tas, 'course': course.name, 'course_slug': course_slug}
+    context = {'tas': tas, 
+                'course': course.name, 
+                'course_slug': course_slug
+                }
     return render(request, 'ta/all_tugs.html', context)
 
 @requires_course_staff_by_slug    
@@ -30,7 +33,17 @@ def new_tug(request, course_slug):
 
 @requires_course_staff_by_slug    
 def view_tug(request, course_slug, userid):
-    return HttpResponse('View TUG page')
+    course = get_object_or_404(CourseOffering, slug=course_slug)
+    member = get_object_or_404(Member, offering=course, person__userid=userid)
+    tug = TUG.objects.get(member=member)
+    max_hours = tug.base_units * 42
+    total_hours = 0 
+    for field, params in tug.config.iteritems():
+        if not field == 'other2':
+            total_hours += params['total']
+    
+    context = {'tug': tug, 'ta':member, 'course':course, 'maxHours':max_hours, 'totalHours':total_hours}
+    return render(request, 'ta/view_tug.html',context)
 
 @requires_course_staff_by_slug
 def edit_tug(request, course_slug, userid):
