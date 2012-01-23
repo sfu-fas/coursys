283c61fa37551f844051fe53fe0158c008721d64
Capstone Student spw4 <spw4@sfu.ca>
2012-01-23 01:04:42 +0000

    minor adjustment to the input fields to allow nulls.

--- grad/models.py
+++ grad/models.py
@@ -20,20 +20,20 @@ CAMPUS_CHOICES = (
 class GradStudent(models.Model):
     person = models.ForeignKey(Person, unique=True)    
     degree_program = models.CharField('Degree Program', max_length=50)
-    starting_semester = models.ForeignKey(Semester, related_name='grad_starting_semester')
-    ending_semester = models.ForeignKey(Semester, related_name='grad_ending_semester')
-    research_area = models.CharField('Research Area', max_length=300)
-    card_access_num = models.IntegerField()
+    starting_semester = models.ForeignKey(Semester, related_name='grad_starting_semester', blank=True, null=True,)
+    ending_semester = models.ForeignKey(Semester, related_name='grad_ending_semester', blank=True, null=True)
+    research_area = models.CharField('Research Area', max_length=200, blank=True)
+    card_access_num = models.IntegerField(null=True, blank=True)
     
 #### change to a db table instead
-    campus = models.CharField(max_length=5, choices=CAMPUS_CHOICES)
+    campus = models.CharField(max_length=5, choices=CAMPUS_CHOICES, blank=True)
     
 #### change to list of instructors instead
-    supervisor_potential = models.CharField('Potential Supervisor', max_length = 100)
-    supervisor_senior = models.CharField('Senior Supervisor', max_length = 100)
-    supervisor_second = models.CharField('Second Supervisor', max_length = 100)
-    supervisor_thrid = models.CharField('Third Supervisor', max_length = 100)
-    supervisor_fourth = models.CharField('Fourth Supervisor', max_length = 100)
+    supervisor_potential = models.CharField('Potential Supervisor', max_length = 100, blank=True)
+    supervisor_senior = models.CharField('Senior Supervisor', max_length = 100, blank=True)
+    supervisor_second = models.CharField('Second Supervisor', max_length = 100, blank=True)
+    supervisor_thrid = models.CharField('Third Supervisor', max_length = 100, blank=True)
+    supervisor_fourth = models.CharField('Fourth Supervisor', max_length = 100, blank=True)
     
 #    def __unicode__(self):
 #        return self.grad   
