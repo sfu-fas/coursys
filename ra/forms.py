da3ab5632f42e7ed30b0f3367f91735eca6edf76
Brendan O'Laney <bolaney@sfu.ca>
2012-02-02 21:32:09 +0000

    added filtering for hiring faculty

--- ra/forms.py
+++ ra/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from ra.models import RAAppointment, Account, Project
-from coredata.models import Person
+from coredata.models import Person, Role
 
-HIRING_FACULTY_CHOICES = [(p.emplid, (p.last_name + ", " + p.first_name)) for p in Person.objects.all()]
+HIRING_FACULTY_CHOICES = [(p.userid, (p.last_name + ", " + p.first_name)) \
+    for p in Person.objects.all() \
+    if Role.objects.filter(person__userid=p.userid, role="FUND").count() > 0]
 
 class RAAppointmentForm(forms.Form):
     hiring_faculty = forms.ChoiceField(choices = HIRING_FACULTY_CHOICES)
