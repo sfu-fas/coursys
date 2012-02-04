30926c6a14c10bdbc1e16b81b77683a3a704a162
Greg Baker <ggbaker@sfu.ca>
2012-02-04 00:29:24 +0000

    copy importer updates to trunk

---
+++ server-setup/do-import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+. $HOME/sqllib/db2profile
+LANG="en_CA.UTF-8" # DB2 doesn't return UTF-8 strings otherwise
+export LANG
+
+cd /home/ggbaker/courses/
+time python coredata/importer-rodb.py < /home/ggbaker/dbpass
