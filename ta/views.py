dd4847cc0de4b906991e4d72ffbd0b61dcbe07c4
Capstone Student jjk5 <jjk5@sfu.ca>
2012-03-01 20:16:40 +0000

    TUG: cleaning up views

--- ta/views.py
+++ ta/views.py
@@ -26,23 +26,8 @@ def index_page(request, course_slug):
         #~ return render(request, 'ta/index.html',{})
     #~ else:
         #~ return ForbiddenResponse(request)
-        
-@login_required
-def all_tugs(request, course_slug):
-    if is_course_staff_by_slug(request, course_slug):
-        return _all_tugs_staff(request, course_slug)
-    # to redirect department admins if they somehow accessed all tugs from courses page
-    elif has_role("ADMN",request):
-        return _all_tugs_admin(request)
-    else:
-        return ForbiddenResponse(request)
 
-# wrapper function for url mapping
-@requires_role("ADMN")
-def all_tugs_admin(request):
-    return _all_tugs_admin(request)
-
-# zip tas and tugs together
+# helps zip tas and tugs together
 # basically performs a left outer join between tas and tugs
 def tryget(member):
     try:
@@ -50,33 +35,36 @@ def tryget(member):
     except(TUG.DoesNotExist):
         return None
     
-#@requires_course_staff_by_slug
-def _all_tugs_staff(request, course_slug):
+@requires_course_staff_by_slug
+def all_tugs(request, course_slug):
     course = get_object_or_404(CourseOffering, slug=course_slug)
     tas = Member.objects.filter(offering=course, role="TA")
+    #TODO: maybe use filter?
     current_user = Member.objects.get(person__userid=request.user.username, offering=course)
     #If a TA is accessing, only his/her own TUG should be viewable
-    not_ta = True;
-    if current_user in tas:
-        tas = tas.filter(person__userid=current_user.person.userid)
-        not_ta = False;
+#    is_ta = current_user in tas
+    is_ta = current_user.role == 'TA'
+    if is_ta:
+        # TODO: just redirect if the user is just a TA (ask in scrumeeting)
+        tas = [current_user]
+        #tas = tas.filter(person__userid=current_user.person.userid)
     tas_with_tugs = [(ta, tryget(ta)) for ta in tas]
     
     context = {
            'tas_with_tugs':tas_with_tugs,
            'course':course,
-           'not_ta':not_ta
+           'not_ta':not is_ta
             }
     
     return render(request, 'ta/all_tugs.html', context)
         
-#@requires_role("ADMN")
-def _all_tugs_admin(request):
+@requires_role("ADMN")
+def all_tugs_admin(request):
     unit = Role.objects.get(person__userid=request.user.username).unit
     courses = CourseOffering.objects.filter(owner=unit) # TO DO: Make this reference the CourseOffering's "Owner" field once it's been added
     tas = Member.objects.filter(offering__in=courses, role="TA")
     tas_with_tugs = [{'ta':ta, 'tug':tryget(ta)} for ta in tas]
-    print tas_with_tugs
+    
     context = {
                'tas_with_tugs':tas_with_tugs,
                'unit':unit,
@@ -93,32 +81,32 @@ def _all_tugs_admin(request):
 def new_tug(request, course_slug, userid):
     course = get_object_or_404(CourseOffering, slug=course_slug)
     member = get_object_or_404(Member, offering=course, person__userid=userid)
-    curr_user_role = Member.objects.get(person__userid=request.user.username,offering=course).role
+#    curr_user_role = Member.objects.get(person__userid=request.user.username,offering=course).role
     
     # TAs should not be creating TUGs
-    if(curr_user_role =="TA" and not userid==request.user.username ): 
-        return ForbiddenResponse(request)
-    else:
-        # Nothing is done as of now until further details as to how to "pre-fill" 
-        # #158    TUG: courses with lab sections should be pre-filled as appropriate
-        has_lab_or_tut = course.labtut()
-            
-        if request.method == "POST":
-            form = TUGForm(data=request.POST, offering=course,userid=userid)
-            if form.is_valid():
-                tug = form.save(False)
-                tug.save(newsitem_author=Person.objects.get(userid=request.user.username))
-                return HttpResponseRedirect(reverse(view_tug, args=(course.slug, userid)))
-        else:
-            form = TUGForm(offering=course,userid=userid)
+#    if(curr_user_role =="TA" and not userid==request.user.username ): 
+#        return ForbiddenResponse(request)
+#    else:
+    # Nothing is done as of now until further details as to how to "pre-fill" 
+    # #158    TUG: courses with lab sections should be pre-filled as appropriate
+    has_lab_or_tut = course.labtut()
         
-        context = {'ta':member.person,
-                   'course':course,
-                   'form':form,
-                   'userid':userid,
-                   #'hasLabOrTut': has_lab_or_tut
-                   }
-        return render(request,'ta/new_tug.html',context)
+    if request.method == "POST":
+        form = TUGForm(data=request.POST, offering=course,userid=userid)
+        if form.is_valid():
+            tug = form.save(False)
+            tug.save(newsitem_author=Person.objects.get(userid=request.user.username))
+            return HttpResponseRedirect(reverse(view_tug, args=(course.slug, userid)))
+    else:
+        form = TUGForm(offering=course,userid=userid)
+    
+    context = {'ta':member.person,
+               'course':course,
+               'form':form,
+               'userid':userid,
+               #'hasLabOrTut': has_lab_or_tut
+               }
+    return render(request,'ta/new_tug.html',context)
 
 @requires_course_staff_or_dept_admn_by_slug    
 def view_tug(request, course_slug, userid):
