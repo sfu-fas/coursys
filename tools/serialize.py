b2cb33c97777ecc6b1b29bab8f5c9507fac27c8a
Brendan O'Laney <bolaney@sfu.ca>
2012-03-19 21:40:28 +0000

    fixed test data for real this time.

--- tools/serialize.py
+++ tools/serialize.py
@@ -3,9 +3,9 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 sys.path.append('.')
 
 # create "objs" by selecting all objects you want to serialize
-from ra.models import *
+from grad.models import *
 
-objs = itertools.chain(RAAppointment.objects.all())
+objs = itertools.chain(Scholarship.objects.all())
 
 # output the JSON: copy into test_data.json when you're sure it's right.
 from django.core import serializers
