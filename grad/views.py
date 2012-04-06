fbaae54991d89a7fe85c49e5f1fb76e49837a9c2
Capstone Student jjk5 <jjk5@sfu.ca>
2012-04-06 10:34:26 +0000

    Grad: search: some final tweaks

--- grad/views.py
+++ grad/views.py
@@ -785,28 +785,45 @@ def view_letter(request, letter_slug):
                }
     return render(request, 'grad/view_letter.html', context)
 
+def get_cleaned_get(request):
+    cleaned_get = copy.copy(request.GET)
+    for k, l in request.GET.iterlists():
+        if len(filter(lambda x:len(x) > 0, l)) == 0:
+            del cleaned_get[k]
+    return cleaned_get
 
 @requires_role("GRAD")
 def search(request):
-    # clean out empty query args, mainly for debugging so it's easier to see what
-    # search arguments are being passed
-    if len(request.GET) > 0:
-        cleaned_get = copy.copy(request.GET)
-        for k, l in request.GET.iterlists():
-            if len(filter(lambda x:len(x) > 0, l)) == 0:
-                del cleaned_get[k]
-        if len(cleaned_get) < len(request.GET):
-            return HttpResponseRedirect(reverse(search) + u'?' + cleaned_get.urlencode())
-    # TODO: move the above code to a separate function
-    
-    if len(request.GET) == 0:
-        form = SearchForm()
-    else:
-        form = SearchForm(request.GET)
-    
+    # Possible TODOs for search:
+    # TODO: make groups of search fields collapsible
+        # use field lists like SearchForm.semester_range_fields to organize the fields
+        # into groups, like Dates, Student Status, Academics, Financials and Personal Details
+        # and put the groups into separate divs, with headers, and use jquery.collapsible
+        # on each of the groups
+        # also this should allow the user to replace the loaded savedsearch with a new one
+    # TODO: allow loading a saved search into the search form
+        # make a new view for this purpose, or separate the results view into its own
+        # just 'copy' (aka refactor by splitting up and follow DRY) this search view to just 
+        # load the searchform with the savedsearch query as initial
     current_user = Person.objects.get(userid=request.user.username)
+    query_string = request.META.get('QUERY_STRING','')
+    try:
+        savedsearch = SavedSearch.objects.get(person=current_user, query=query_string)
+    except SavedSearch.DoesNotExist:
+        savedsearch = None
+    if savedsearch is None:
+        if len(request.GET) > 0:
+            cleaned_get = get_cleaned_get(request)
+            if len(cleaned_get) < len(request.GET):
+                return HttpResponseRedirect(reverse(search) + u'?' + cleaned_get.urlencode())
+        try:
+            savedsearch = SavedSearch.objects.get(person=current_user, query=query_string)
+        except SavedSearch.DoesNotExist:
+            savedsearch = None
+    
+    form = SearchForm() if len(request.GET) == 0 else SearchForm(request.GET)
+    
     if form.is_valid():
-        
         query = form.get_query()
         grads = GradStudent.objects.filter(query).distinct()
         grads = filter(form.secondary_filter(), grads)
@@ -814,11 +831,9 @@ def search(request):
         #grads = itertools.ifilter(form.secondary_filter, grads)
         # TODO get list of columns selected and send that seperately
         
-        # if we store the query string in the database 
-        query_string = request.META.get('QUERY_STRING','')
-        try:
-            saveform = SaveSearchForm(instance=SavedSearch.objects.get(person=current_user, query=query_string))
-        except SavedSearch.DoesNotExist:
+        if savedsearch is not None:
+            saveform = SaveSearchForm(instance=savedsearch)
+        else:
             saveform = SaveSearchForm(initial={'person':current_user, 'query':query_string})
         
         columns = form.cleaned_data['columns']
@@ -834,7 +849,10 @@ def search(request):
         context = {
                    'savedsearches' : savedsearches,
                    'page_title' : page_title,
-                   'form':form
+                   'form':form,
+                   'savedsearch' : savedsearch 
+                   # a non-None savedsearch here means that somehow, an invalid search got saved
+                   # the template gives the user the option to delete it
                    }
         return render(request, 'grad/search.html', context)
 
