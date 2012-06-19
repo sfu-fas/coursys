ffe8fb88e42afce50db9d8fd137f39e540998371
Mark Westerhof <mdw4@sfu.ca>
2012-06-19 18:58:18 +0000

    Fixed nonetype error when aggregating discussion topics when there isn't any

--- discuss/activity.py
+++ discuss/activity.py
@@ -8,8 +8,10 @@ def recent_activity(member):
     """
     Returns whether there has been any discussion activity since last visited for a member
     """
-    latest_activity = DiscussionTopic.objects.filter(offering=member.offering).aggregate(Max('last_activity_at'))
-    return datetime.datetime.fromtimestamp(member.last_discuss()) < latest_activity['last_activity_at__max']
+    latest_activity = DiscussionTopic.objects.filter(offering=member.offering).aggregate(Max('last_activity_at'))['last_activity_at__max']
+    if latest_activity is None:
+        latest_activity = datetime.datetime.fromtimestamp(0)
+    return datetime.datetime.fromtimestamp(member.last_discuss()) < latest_activity
 
 def update_last_viewed(member):
     """
