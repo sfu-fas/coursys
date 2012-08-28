f14fe8575edfe9781259ad7719b3fd082e88e061
Curtis Lassam <curtis@lassam.net>
2012-08-28 16:12:54 -0700

    Provisioning code no longer loads test data. It's better to do it manually.

--- virtual_machine/chef-repo/cookbooks/coursys/recipes/default.rb
+++ virtual_machine/chef-repo/cookbooks/coursys/recipes/default.rb
@@ -10,10 +10,3 @@ execute "install_pip_requirements" do
     cwd "/home/vagrant/"
     command "pip install -r /home/vagrant/courses/build_deps/working_deps.txt"
 end
-
-# python manage.py syncdb, migrate, load data
-execute "syncdb" do
-    cwd "/home/vagrant/courses"
-    command "tools/reset_db.sh"
-end
-
