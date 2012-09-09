7e131457496ac009330575e78d84521d18f56291
pzenger <peter.zenger1@gmail.com>
2012-09-09 16:37:12 -0700

    Testing if new branch works, + early brainstorming

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+"""
+class Event:
+	#start_time
+	#end_time
+	#owner
+
+class Appointment:
+	#event
+	#date
+	#time
+	#Need to duplicate date/time since appointments can never change
+
+
+class Recurrence:
+	#days = 
+	#recurring = boolean
+	#owner = advisor
+	
+	#end_date
+	#start_date
+
+	#start_time
+	#end_time
+
+	#location
+	#type, like bookable or not
+"""
