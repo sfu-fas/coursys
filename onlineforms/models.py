16e226428354252ca21ec9d66c02cc678fc3afb2
Greg Baker <ggbaker@sfu.ca>
2012-09-18 15:40:28 -0700

    choices fields for onlineforms models

--- onlineforms/models.py
+++ onlineforms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# choices for Form.initiator field
+INITIATOR_CHOICES = [
+        ('LOG', 'Logged-in SFU users'),
+        ('ANY', 'Anyone, including non-SFU users'),
+        # may add others if needed, e.g. instructors, admin staff, majors in a specific program, ...
+        ]
+
+FIELD_TYPE_CHOICES = [
+        ('SMTX', 'Small Text (single line)'),
+        ('MDTX', 'Large Text (a few lines)'),
+        ('LGTX', 'Large Text (many lines)'),
+        ('EMAI', 'Email address'),
+        ('RADI', 'Select with radio buttons'),
+        ('SEL1', 'Select with a drop-down menu'),
+        ('SELN', 'Select multiple values'),
+        ('LIST', 'Enter a list of short responses'),
+        ('FILE', 'Upload a file'),
+        ('URL', 'A web page address (URL)'),
+        ]
