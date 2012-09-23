8b1ca23c8eb205b782a2245953095624a3380456
Greg Baker <ggbaker@sfu.ca>
2012-09-23 00:30:06 -0700

    more methods I see us needing for FieldTypes

--- onlineforms/fieldtypes/base.py
+++ onlineforms/fieldtypes/base.py
@@ -1,6 +1,16 @@
 # base models for FieldType classes
 
 class FieldType(object):
+    def edit_form_factory(self):
+        """
+        Returns a Django Form instance that can be used to edit this
+        field's details.
+        
+        e.g. user might want to edit the things in the .config field:
+        this Form lets them.
+        """
+        raise NotImplementedError
+        
     def dispay_widget(self, filled=None):
         """
         Returns HTML for this field, to be filled in by the user. Should
@@ -20,3 +30,10 @@ class FieldType(object):
         """
         raise NotImplementedError
 
+    def is_complete(self, filled):
+        """
+        Returns True if the filled-in value is "complete". i.e. if this
+        field is required, is it "done"?
+        """
+        return filled.is_complete()
+        
