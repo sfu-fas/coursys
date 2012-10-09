8eb479427ee1cc3b7c520cba5926f9b80d3c93d2
pzenger <peter.zenger1@gmail.com>
2012-10-08 23:54:26 -0700

    Cannot get an import to work properly

--- onlineforms/views.py
+++ onlineforms/views.py
@@ -6,8 +6,9 @@ from django.core.urlresolvers import reverse
 from courselib.auth import NotFoundResponse, ForbiddenResponse, requires_role
 
 # FormGroup management views
-from onlineforms.forms import ConfigFieldForm
-from onlineforms.models import Form, Sheet, Field
+from onlineforms.forms import FieldForm
+from onlineforms.fieldtypes import *
+from onlineforms.models import Form, Sheet, Field, FIELD_TYPE_MODELS
 
 def manage_groups(request):
     pass
@@ -58,31 +59,61 @@ def new_field(request, form_slug, sheet_slug):
     #Test url: http://localhost:8000/forms/comp-test-form-1/edit/initial-sheet/new
     owner_form = get_object_or_404(Form, slug=form_slug) #Maybe only owner_sheet is needed
     owner_sheet = get_object_or_404(Sheet, slug=sheet_slug)
+    section = 'setup'
+
+
     print owner_sheet
 
     if request.method == 'POST':
-        form = ConfigFieldForm(request.POST)
-        if form.is_valid():
-            custom_config = {}
+        if 'next_section' in request.POST:
+            section = request.POST['next_section']
+        #Start with choosing the form in setup, then the forms config after
+        if section == 'setup':
+            form = FieldForm(request.POST)
+            if form.is_valid():
 
-            Field.objects.create(label=form.cleaned_data['name'],
-                sheet=owner_sheet,
-                required=form.cleaned_data['required'],
-                fieldtype=form.cleaned_data['type'],
-                config=custom_config,
-                active=True,
-                original=None,
-                created_date=datetime.now(),
-                last_modified=datetime.now())
+                custom_config = {}# = json.dump(list(all_events), resp, indent=1)
 
-            messages.success(request, 'Successfully created the new field \'%s\'' % form.cleaned_data['name'])
+                #Field.objects.create(label=form.cleaned_data['name'],
+                #    sheet=owner_sheet,
+                #    required=form.cleaned_data['required'],
+                #    fieldtype=form.cleaned_data['type'],
+                #    config=custom_config,
+                #    active=True,
+                #    original=None,
+                #    created_date=datetime.now(),
+                #    last_modified=datetime.now())
+
+                messages.success(request, 'Successfully created the new field \'%s\'' % form.cleaned_data['name'])
+            else:
+                print "Invalid"
         else:
-            print "Invalid"
+            #forms config section
+            type = request.POST['type']
+            type = FIELD_TYPE_MODELS[type]
+
+
+
+            field = type()
+            print field
+            field.
+            #form = field.make_config_form()
+            form = field.make_config_form()
+            #config_form = field.make_config_form()
+
+            #print config_form
+
+            #field =
+            #form = type.make_config_form()
+            #print form
+            form = FieldForm()
+            print "type: "+str(type)
 
     else:
-        form = ConfigFieldForm()
+        form = FieldForm()
+    print "section" + section
 
-    context = {'form': form, 'slug_form': form_slug, 'slug_sheet': sheet_slug}
+    context = {'form': form, 'slug_form': form_slug, 'slug_sheet': sheet_slug, 'section':section}
     return render(request, 'onlineforms/new_field.html', context)
 
 
