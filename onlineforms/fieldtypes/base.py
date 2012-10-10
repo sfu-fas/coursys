bb3b0c7bce1afdcdc457ac7e26bef60dd5dd5c3a
pzenger <peter.zenger1@gmail.com>
2012-10-09 22:26:16 -0700

    Config is nice and clean now, cleaned up code

--- onlineforms/fieldtypes/base.py
+++ onlineforms/fieldtypes/base.py
@@ -3,7 +3,7 @@ from django import forms
 
 # base models for FieldType classes
 
-class FieldConfigForm( forms.Form ):
+class FieldConfigForm(forms.Form):
     """
     The base form for field configuration.
 
@@ -11,9 +11,13 @@ class FieldConfigForm( forms.Form ):
     all field objects.
 
     """
-    required = forms.BooleanField( label="Required")
-    label = forms.CharField( label="Label" )
-    help_text = forms.CharField( label="Help Text" )
+    RADIO_CHOICES = (
+        ('yes', 'Required'),
+        ('no', 'Not required'),
+        )
+    required = forms.BooleanField(label="Required", required=False)
+    label = forms.CharField(label="Label")
+    help_text = forms.CharField(label="Help Text")
 
     def serialize(self):
         """
@@ -22,7 +26,8 @@ class FieldConfigForm( forms.Form ):
         """
         return self.cleaned_data
 
-class FieldBase( object ):
+
+class FieldBase(object):
     default_config = {'required': False, 'label': '', 'help_text': ''}
 
     def __init__(self, config=None):
@@ -48,7 +53,7 @@ class FieldBase( object ):
         this Form lets them.
         """
         raise NotImplementedError
-        
+
     def make_entry_field(self, fieldsubmission=None):
         """
         Returns a Django Field for this field, to be filled in by the
@@ -58,7 +63,7 @@ class FieldBase( object ):
         e.g. a CharField field.
         """
         raise NotImplementedError
-    
+
     def serialize_field(self, field):
         """
         Convert filled field (as returned by make_entry_field)
