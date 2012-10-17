edccd843ddf944db5c1777063c03cc1172c5636b
Curtis Lassam <curtis@lassam.net>
2012-10-16 18:08:35 -0700

    Added a 'created date' datetime column to the letters table. Switched Yours Truly with Sincerely.

--- grad/models.py
+++ grad/models.py
@@ -604,7 +604,7 @@ class Letter(models.Model):
     content = models.TextField(help_text="I.e. 'This is to confirm Mr. Baker ... '")
     template = models.ForeignKey(LetterTemplate)
     salutation = models.CharField(max_length=100, default="To whom it may concern", blank=True)
-    closing = models.CharField(max_length=100, default="Yours truly")
+    closing = models.CharField(max_length=100, default="Sincerely")
     from_person = models.ForeignKey(Person, null=True)
     from_lines = models.TextField(help_text='Name (and title) of the signer, e.g. "John Smith, Program Director"')
     
