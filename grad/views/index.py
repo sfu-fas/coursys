d4b4311a1d15d715779fcc10477c85ec60dea271
Greg Baker <ggbaker@sfu.ca>
2012-10-28 13:48:24 -0700

    limit saved searches to others in same unit(s)

--- grad/views/index.py
+++ grad/views/index.py
@@ -1,4 +1,5 @@
 from courselib.auth import requires_role
+from coredata.models import Role
 from django.shortcuts import render
 from grad.forms import QuickSearchForm
 from grad.models import SavedSearch
@@ -7,6 +8,7 @@ from grad.models import SavedSearch
 def index(request):
     form = QuickSearchForm()
     savedsearches = SavedSearch.objects.filter(person__userid=(request.user.username))
-    other_savedsearches = SavedSearch.objects.exclude(person__userid=(request.user.username))
+    other_gradadmin = [r['person_id'] for r in Role.objects.filter(role="GRAD", unit__in=request.units).values('person_id')]
+    other_savedsearches = SavedSearch.objects.filter(person__in=other_gradadmin).exclude(person__userid=(request.user.username))
     context = {'units': request.units, 'form': form, 'savedsearches': savedsearches, 'other_savedsearches':other_savedsearches}
     return render(request, 'grad/index.html', context)
