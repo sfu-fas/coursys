ae8e3630a1cf40071bfd45ef0297f96d97d24464
Greg Baker <ggbaker@sfu.ca>
2012-12-18 11:18:27 -0800

    fix coredata test for yet another case

--- coredata/tests.py
+++ coredata/tests.py
@@ -237,7 +237,7 @@ class CoredataTest(TestCase):
         response = client.post(url, {'person':'33333333', 'role':'FAC'})
         self.assertEquals(response.status_code, 200)
         validate_content(self, response.content, url)
-        self.assertTrue("could not connect to reporting database" in response.content or "Could not find this emplid." in response.content)
+        self.assertTrue("could not import DB2 module" in response.content or "could not connect to reporting database" in response.content or "Could not find this emplid." in response.content)
 
         response = client.post(url, {'person':p1.emplid, 'role':'FAC', 'unit':unit.id})
         self.assertEquals(response.status_code, 302)
