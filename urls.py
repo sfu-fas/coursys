722380ee4141c01fcf19dcc2390f1d7f91d9ddc7
Curtis Lassam <curtis@lassam.net>
2013-09-30 14:26:27 -0700

    Alerts now has its own urls.py

--- urls.py
+++ urls.py
@@ -481,21 +481,8 @@ urlpatterns += patterns('',
     url(r'^forms/', include('onlineforms.urls')),
     
     # Alerts
+    url(r'^alerts/', include('alerts.urls')),
 
-    url(r'^alerts/new_alerts/$', 'alerts.views.rest_alerts'),
-    url(r'^alerts/$', 'alerts.views.view_alert_types'),
-    url(r'^alerts/send/$', 'alerts.views.send_emails'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/$', 'alerts.views.view_alerts'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/(?P<alert_id>\d+)/$', 'alerts.views.view_alert'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/resolved/$', 'alerts.views.view_resolved_alerts'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/all/$', 'alerts.views.view_all_alerts'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/(?P<alert_id>\d+)/resolve', 'alerts.views.resolve_alert'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/(?P<alert_id>\d+)/reopen', 'alerts.views.reopen_alert'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/(?P<alert_id>\d+)/comment', 'alerts.views.comment_alert'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/automation/$', 'alerts.views.view_automation'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/automation/new/$', 'alerts.views.new_automation'),
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/automation/(?P<automation_id>\d+)/(?P<alert_id>\d+)/$', 'alerts.views.view_email_preview' ),    
-    url(r'^alerts/type/' + ALERT_TYPE_SLUG + '/automation/(?P<automation_id>\d+)/delete/$', 'alerts.views.delete_automation' ),    
 )
 
 if not settings.DEPLOYED:
