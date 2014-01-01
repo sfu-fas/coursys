089de693888f9e813af84d0973cd62ba665f26e9
Greg Baker <ggbaker@sfu.ca>
2014-01-01 11:06:12 -0800

    minor updates to faculty browser

--- coredata/models.py
+++ coredata/models.py
@@ -835,10 +835,10 @@ class Member(models.Model):
             if joint_with > 1:
                 credits /= joint_with
                 reasons.append('joint with %i other' % (joint_with-1))
-            if self.offering.units is not None and self.offering.units != 3:
-                # TODO: is this consistent with other units on campus? Do some have a different default credit count?
-                credits *= fractions.Fraction(self.offering.units, 3)
-                reasons.append('%i unit course' % (self.offering.units))
+            #if self.offering.units is not None and self.offering.units != 3:
+            #    # TODO: is this consistent with other units on campus? Do some have a different default credit count?
+            #    credits *= fractions.Fraction(self.offering.units, 3)
+            #    reasons.append('%i unit course' % (self.offering.units))
             return credits, ', '.join(reasons)
 
     def set_teaching_credit(self, cred):
