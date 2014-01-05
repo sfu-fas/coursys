a7ea30a23d820c53d29cb0e6698378371ec279c5
Greg Baker <ggbaker@sfu.ca>
2014-01-05 14:40:40 -0800

    celerybeat deployment setup

--- coredata/tasks.py
+++ coredata/tasks.py
@@ -9,6 +9,8 @@ def update_repository_task(*args, **kwargs):
 
 # some tasks for testing/experimenting
 import time
+from celery.task import periodic_task
+from celery.task.schedules import crontab
 
 @task(queue='fast')
 def ping():
@@ -17,4 +19,11 @@ def ping():
 @task(rate_limit='60/m')
 def slow_task():
     #time.sleep(5)
+    #print "HELLO SLOW TASK"
     return True
+
+#@periodic_task(run_every=crontab())
+#def test_periodic_task():
+#    print "HELLO PERIODIC TASK"
+#    return True
+
