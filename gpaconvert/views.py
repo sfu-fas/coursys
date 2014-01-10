0f46e70d0ec862068a6327eacea22c760f8b3cc6
Greg Baker <ggbaker@sfu.ca>
2014-01-10 12:06:52 -0800

    basic view code

--- gpaconvert/views.py
+++ gpaconvert/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404
+
+from gpaconvert.models import GradeSource, DiscreteRule, ContinuousRule
+
+# admin interface views
+
+
+# student-facing views
