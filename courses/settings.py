5b64363220d640dcf17e6d473d0ba917b4491f74
Greg Baker <ggbaker@sfu.ca>
2014-01-13 15:51:49 -0800

    small changes for static media updates

--- courses/settings.py
+++ courses/settings.py
@@ -81,6 +81,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
     "django.core.context_processors.media",
     "django.contrib.messages.context_processors.messages",
     'django.core.context_processors.request',
+    'django.core.context_processors.static',
     'dashboard.context.media',
     )
 
