ff6b7e3065211ff3a8739eb4afa35ccccfa891de
Michael Tom-Wing <mtomwing@gmail.com>
2014-01-13 20:39:26 -0800

    Added stub for the convert grades view

--- gpaconvert/urls.py
+++ gpaconvert/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'gpaconvert.views.list_grade_sources'),
+    url(r'^(?P<grade_source_slug>[\d\w-]+)$', 'gpaconvert.views.convert_grades'),
 )
--- gpaconvert/views.py
+++ gpaconvert/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404
+
+from gpaconvert.forms import DiscreteGradeForm
 from gpaconvert.models import GradeSource
 from gpaconvert.utils import render_to
 
@@ -8,4 +11,19 @@ def list_grade_sources(request):
 
     return {
         'grade_sources': grade_sources,
+    }
+
+
+@render_to('gpaconvert/convert_grades_form.html')
+def convert_grades(request, grade_source_slug):
+    grade_source = get_object_or_404(GradeSource, slug=grade_source_slug)
+
+    if request.POST:
+        form = DiscreteGradeForm(request.POST, grade_source=grade_source)
+    else:
+        form = DiscreteGradeForm(grade_source=grade_source)
+
+    return {
+        'grade_source': grade_source,
+        'form': form,
     }
