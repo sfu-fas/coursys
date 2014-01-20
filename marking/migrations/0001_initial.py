0c7af9b20ad3e04026993ccb9f0eb9e084d56669
Greg Baker <ggbaker@sfu.ca>
2014-01-20 12:15:31 -0800

    more migration dependancies

--- marking/migrations/0001_initial.py
+++ marking/migrations/0001_initial.py
@@ -7,6 +7,11 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("grades", "0001_initial"),
+        ("groups", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'ActivityComponent'
         db.create_table('marking_activitycomponent', (
