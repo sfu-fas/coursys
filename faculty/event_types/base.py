30cae1258b6bca4b0fb2348351275c78c3663ba0
Michael Tom-Wing <mtomwing@gmail.com>
2014-02-01 20:44:53 -0800

    Added Choices, an ordered-dict that works with field choices

--- faculty/event_types/base.py
+++ faculty/event_types/base.py
@@ -280,3 +280,18 @@ class CareerEventHandlerBase(object):
 
         """
         pass
+
+
+class Choices(collections.OrderedDict):
+    '''
+    An ordered dictionary that also acts as an iterable of (key, value) pairs.
+    '''
+
+    def __init__(self, *choices):
+        super(Choices, self).__init__(choices)
+
+    def __iter__(self):
+        # XXX: Can't call super(Choices, self).iteritems() here because it will call our
+        #      __iter__ and recurse infinitely.
+        for key in super(Choices, self).__iter__():
+            yield (key, self[key])
