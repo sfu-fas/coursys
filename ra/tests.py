bf9bbbf1e5bdea3a3351d1c4cfe6cdc766d96336
Greg Baker <ggbaker@sfu.ca>
2014-02-24 12:42:19 -0800

    some basic tests for ra module
    Former-commit-id: 0aa441fbbe18e509ae8d567ba8e72396e6c90f0a

--- ra/tests.py
+++ ra/tests.py
@@ -1,16 +1,30 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from courselib.testing import Client, test_views
+from ra.models import RAAppointment, Account, Project
+
+class RATest(TestCase):
+    fixtures = ['test_data']
+    def test_pages(self):
+        """
+        Test basic page rendering
+        """
+        c = Client()
+        c.login_user('ggbaker')
+
+        test_views(self, c, 'ra.views.', ['search', 'new', 'accounts_index', 'new_account', 'projects_index',
+                                          'new_project', 'semester_config', 'browse'], {})
+        test_views(self, c, 'ra.views.', ['found'], {}, qs='search=grad')
+
+        ra = RAAppointment.objects.filter(unit__label='CMPT')[0]
+        p = ra.person
+        test_views(self, c, 'ra.views.', ['student_appointments', 'new_student'], {'userid': p.userid})
+        test_views(self, c, 'ra.views.', ['edit', 'reappoint', 'edit_letter', 'view',], {'ra_slug': ra.slug})
+
+        acct = Account.objects.filter(unit__label='CMPT')[0]
+        test_views(self, c, 'ra.views.', ['edit_account'], {'account_slug': acct.slug})
+
+        proj = Project.objects.filter(unit__label='CMPT')[0]
+        test_views(self, c, 'ra.views.', ['edit_project'], {'project_slug': proj.slug})
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
