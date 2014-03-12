8d4f71a5badfa7a7e9d7b63c1ef11d9753110216
sebastiengalina <sebastiengalina1991@gmail.com>
2014-03-11 17:40:23 -0700

    added tempgrant model
    Former-commit-id: a8389b7ec46e303e61dd27376e01880d914633d6

--- faculty/models.py
+++ faculty/models.py
@@ -436,10 +436,42 @@ class EventConfig(models.Model):
     config = JSONField(default={})
 
 
+class TempGrantManager(models.Manager): 
+    def create_from_csv(self, file):
+        with file.open(mode='rb') as csvfile:
+            # TODO: do we have a csv file to play with, or should we use the XLS greg gave us?
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for row in rows:
+                # ----------------------------
+                # DO SOME SERIOUS PARSING HERE
+                # ----------------------------
+                print row
+                # create TempGrant from row
+
+
+class TempGrant(models.Model):
+    label = models.CharField(max_length=255, help_text="for identification from FAST import")
+    project_code = models.CharField(max_length=32, help_text="The fund and project code, like '13-123456'")
+    initial = models.DecimalField(verbose_name="initial balance", max_digits=12, decimal_places=2)
+    overhead = models.DecimalField(verbose_name="annual overhead", max_digits=12, decimal_places=2)
+    import_key = models.CharField(null=True, blank=True, max_length=255, help_text="e.g. 'nserc-43517b4fd422423382baab1e916e7f63'")
+    config = JSONField(default={}) # addition configuration for within the temp grant
+
+    def convert_to_grant(self, **kwargs):
+        data = {
+            "label": self.label,
+            "project_code": self.project_code,
+            "initial": self.initial,
+            "overhead": self.overhead,
+            "import_key": self.import_key,
+            "config": self.config,
+        }
+        data.update(**kwargs)
+        grant = Grant(**data)
+        return grant
+
+
 class GrantManager(models.Manager):
-    def create_from_csv(self, row):
-        raise NotImplementedError
-    
     def active(self):
         qs = self.get_query_set()
         return qs.filter(status='A')
@@ -453,7 +485,7 @@ class Grant(models.Model):
     title = models.CharField(max_length=64)
     slug = AutoSlugField(populate_from='title', unique_with=("unit",), null=False, editable=False)
     label = models.CharField(max_length=255, help_text="for identification from FAST import", db_index=True)
-    # TODO: owners, ManyToMany or Foreign key via a separate GrantOwner model?
+    owners = models.ManyToManyField(Person, blank=True, null=True)
     project_code = models.CharField(max_length=32, db_index=True, help_text="The fund and project code, like '13-123456'")
     start_date = models.DateField(null=False, blank=False)
     expiry_date = models.DateField(null=True, blank=True)
@@ -462,7 +494,7 @@ class Grant(models.Model):
     overhead = models.DecimalField(verbose_name="annual overhead", max_digits=12, decimal_places=2)
     import_key = models.CharField(null=True, blank=True, max_length=255, help_text="e.g. 'nserc-43517b4fd422423382baab1e916e7f63'")
     unit = models.ForeignKey(Unit, null=False, blank=False, help_text="unit who owns the grant")
-    config = JSONField(default={})  # addition configuration for within the memo
+    config = JSONField(default={})  # addition configuration for within the grant
 
     objects = GrantManager()
 
@@ -472,14 +504,15 @@ class Grant(models.Model):
     def get_absolute_url(self):
         return reverse("view_grant", kwargs={'unit_slug': self.unit.slug, 'grant_slug': self.slug})
 
-    def update_balance(self, date, balance, spent_this_month):
+    def update_balance(self, balance, spent_this_month, date=datetime.datetime.today()):
         gb = GrantBalance.objects.create(
-                date=date,
-                grant=self,
-                balance=balance,
-                actual=self.initial - balance,
-                month=spent_this_month
+            date=date,
+            grant=self,
+            balance=balance,
+            actual=self.initial - balance,
+            month=spent_this_month
         )
+        return gb
 
 
 class GrantBalance(models.Model):
