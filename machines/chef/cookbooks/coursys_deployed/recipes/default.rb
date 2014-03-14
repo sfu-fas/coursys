3d68aca0d52035ed789a28dada0f3ee1678feec8
classam <curtis@lassam.net>
2014-03-13 19:12:30 -0700

    further development on coursys deployed machine
    Former-commit-id: 83b120eb303d49e55e0d869f1aea07dd14ea19e0

--- machines/chef/cookbooks/coursys_deployed/recipes/default.rb
+++ machines/chef/cookbooks/coursys_deployed/recipes/default.rb
@@ -1,10 +1,36 @@
-package "rabbitmq-server"
+# MySQL
 package "mysql-server"
 package "mysql-client"
 package "libmysqlclient-dev"
+
+# Media Server
+package "nginx"
+
+# WSGI Server
+package "gunicorn"
+
+# Queue Server
+package "rabbitmq-server"
+
+# Cache Server
+package "memcached"
+
+# We need this to connect to things in our data center because BLAUGH 
+package "stunnel4"
+
+# Keep the time in sync
+package "ntp"
+
+# To update the code
+package "git"
 
 # pip install any listed requirements
 execute "install_pip_requirements" do
     cwd "/home/vagrant/"
     command "pip install -r /home/vagrant/courses/build_deps/deployed_deps.txt"
 end
+
+# TODO
+#  - move nginx to port 8000
+#  - create startup script to run gunicorn on port 80
+#     gunicorn wsgi --bind 0.0.0.0:80
