da400e42e23fd9f0f2274d4d89b12636e98033bc
Greg Baker <ggbaker@sfu.ca>
2014-03-24 22:52:09 -0700

    fix locale building in recipe
    Former-commit-id: 2244d53cb186817fd6a3db91e3c25e8417394466

--- machines/chef/cookbooks/coursys/recipes/default.rb
+++ machines/chef/cookbooks/coursys/recipes/default.rb
@@ -10,5 +10,9 @@ package "zlib1g-dev"
 execute "install_pip_requirements" do
     cwd "/home/vagrant/"
     command "pip install -r /home/vagrant/courses/build_deps/working_deps.txt"
-    command "sudo locale-gen en_CA.UTF-8"
+end
+
+# build the locale that a few bits of the system rely on
+execute "build_locale" do
+    command "locale-gen en_CA.UTF-8"
 end
