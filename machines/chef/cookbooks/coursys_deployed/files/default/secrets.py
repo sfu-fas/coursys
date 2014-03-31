53eabbad1219334018ddc2d961333be1cf8dc134
Greg Baker <ggbaker@sfu.ca>
2014-03-30 17:43:36 -0700

    disable empty DB config on VM
    Former-commit-id: 836d97410f599168965b2fb7b6c4aed3a993d3da

--- machines/chef/cookbooks/coursys_deployed/files/default/secrets.py
+++ machines/chef/cookbooks/coursys_deployed/files/default/secrets.py
@@ -1,11 +1,11 @@
 # MySQL connection stuff
-DB_CONNECTION = {
-    'NAME': '',
-    'USER': '',
-    'PASSWORD': '',
-    'HOST': '',
-    'PORT': 4000,
-}
+#DB_CONNECTION = {
+#    'NAME': '',
+#    'USER': '',
+#    'PASSWORD': '',
+#    'HOST': '',
+#    'PORT': 4000,
+#}
 
 # 50-random-character secret key for crypto thingies
 SECRET_KEY='THIS IS NOT A VERY SECRET KEY'
