2374b48d2c5087c265f55f08b20b7fcc858983b3
Greg Baker <ggbaker@sfu.ca>
2014-03-31 17:54:17 -0700

    more configuration for a production(-like) environment
    Former-commit-id: 5e53b4d35551a18bd12732cbe5496157d6817404

--- machines/chef/cookbooks/coursys_deployed/recipes/default.rb
+++ machines/chef/cookbooks/coursys_deployed/recipes/default.rb
@@ -23,6 +23,7 @@ package "ntp"
 # Dev tools
 package "vim"
 package "git"
+package "screen"
 
 # pip install any listed requirements
 execute "install_pip_requirements" do
@@ -41,6 +42,49 @@ cookbook_file "secrets.py" do
     path "/home/vagrant/courses/courses/secrets.py"
     action :create
 end
+
+# misc system config
+cookbook_file "stunnel.conf" do
+    path "/etc/stunnel/stunnel.conf"
+end
+cookbook_file "rabbitmq.conf" do
+    path "/etc/rabbitmq/rabbitmq.conf"
+end
+execute "deny_coursys_ssh" do
+    cwd "/"
+    command "grep -q 'DenyUsers coursys'  /etc/ssh/sshd_config || (echo '\nDenyUsers coursys' >> /etc/ssh/sshd_config)"
+end
+
+# celery daemon
+cookbook_file "celeryd-init" do
+    path "/etc/init.d/celeryd"
+    mode 0755
+end
+cookbook_file "celeryd-defaults" do
+    path "/etc/default/celeryd"
+end
+execute "debconf_update" do
+    cwd "/"
+    command "chsh -s /bin/bash www-data"
+end
+
+
+# postfix mail server
+package "postfix"
+cookbook_file "package-config.txt" do
+    path "/tmp/package-config.txt"
+end
+execute "debconf_update" do
+    cwd "/"
+    command "debconf-set-selections /tmp/package-config.txt"
+end
+execute "debconf_reconfigure" do
+    cwd "/"
+    command "rm /etc/postfix/main.cf /etc/postfix/master.cf ; dpkg-reconfigure -f noninteractive postfix"
+end
+
+
+
 
 # configure NGINX
 cookbook_file "nginx_default.conf" do
