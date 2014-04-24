883d167e5c208eb12ff02613ce653885c0ca1d2b
classam <curtis@lassam.net>
2014-04-23 17:39:26 -0700

    slight adjustment to caching behavior
    Former-commit-id: 29c9ee3ef180b51e698d0e0b69fec3705e884941

--- reports/cache.py
+++ reports/cache.py
@@ -2,11 +2,6 @@ from django.conf import settings
 import shutil
 import os
 
-def cache_location():
-    if not os.path.exists(settings.REPORT_CACHE_LOCATION):
-        os.makedirs(settings.REPORT_CACHE_LOCATION)
-    return settings.REPORT_CACHE_LOCATION
-
 def clear_cache():
     """ Remove the contents of /tmp/report_cache """
     if settings.REPORT_CACHE_CLEAR: 
