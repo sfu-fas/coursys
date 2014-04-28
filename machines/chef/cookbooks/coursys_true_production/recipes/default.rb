6f8a9651d6d9c0396237bdc5e1d0280f57be21be
Curtis Lassam <curtis@lassam.net>
2014-04-28 15:52:57 -0700

    added alternate nginx config path to true_production
    Former-commit-id: 9924b0d0754e533300a2e46bea00b4934a83ce74

--- machines/chef/cookbooks/coursys_true_production/recipes/default.rb
+++ machines/chef/cookbooks/coursys_true_production/recipes/default.rb
@@ -16,3 +16,12 @@ execute "debconf_reconfigure" do
     cwd "/"
     command "rm /etc/postfix/main.cf /etc/postfix/master.cf ; dpkg-reconfigure -f noninteractive postfix"
 end
+
+# reconfigure NGINX with end-user properties
+cookbook_file "nginx_default.conf" do
+    path "/etc/nginx/sites-available/default"
+    action :create
+end
+service "nginx" do
+  action :restart
+end
