fefa23847d82e2d315fe063be5b43489bb8522ff
classam <curtis@lassam.net>
2014-09-02 22:41:24 -0700

    wup. have to mock it out in the setup, too

--- api/tests.py
+++ api/tests.py
@@ -22,46 +22,48 @@ VERIFIER = '1234567890'
 
 class APITest(TestCase):
     def setUp(self):
-        self.client = Client()
+        with mock.patch('time.time', 525942870):
+            self.client = Client()
 
-        # create a Consumer (and associated stuff)
-        try:
-            u = User.objects.get(username='ggbaker')
-        except User.DoesNotExist:
-            u = User(username='ggbaker')
-            u.save()
+            # create a Consumer (and associated stuff)
+            try:
+                u = User.objects.get(username='ggbaker')
+            except User.DoesNotExist:
+                u = User(username='ggbaker')
+                u.save()
 
-        try:
-            c = Consumer.objects.get(name='Test Consumer')
-        except Consumer.DoesNotExist:
-            c = Consumer(name='Test Consumer')
+            try:
+                c = Consumer.objects.get(name='Test Consumer')
+            except Consumer.DoesNotExist:
+                c = Consumer(name='Test Consumer')
 
-        c.description = 'Consumer to do some tests with'
-        c.status = ACCEPTED
-        c.user = u
-        c.xauth_allowed = False
-        c.generate_random_codes()
-        c.save()
-        self.consumer = c
+            c.description = 'Consumer to do some tests with'
+            c.status = ACCEPTED
+            c.user = u
+            c.xauth_allowed = False
+            c.generate_random_codes()
+            c.save()
+            self.consumer = c
 
-        i = ConsumerInfo(consumer=c)
-        i.admin_contact = 'the_developer@example.com'
-        i.permissions = ['courses']
-        i.timestamp = time.time() - 10 # make sure the ConsumerInfo was there "before" the Token was created
-        i.save()
-        self.consumerinfo = i
+            i = ConsumerInfo(consumer=c)
+            i.admin_contact = 'the_developer@example.com'
+            i.permissions = ['courses']
+            i.timestamp = time.time() - 10 # make sure the ConsumerInfo was there "before" the Token was created
+            print time.time()
+            i.save()
+            self.consumerinfo = i
 
-        # create an access token so we can jump in to requests
-        try:
-            t = Token.objects.get(token_type=Token.ACCESS, consumer=c, user=u)
-        except Token.DoesNotExist:
-            t = Token(token_type=Token.ACCESS, consumer=c, user=u)
+            # create an access token so we can jump in to requests
+            try:
+                t = Token.objects.get(token_type=Token.ACCESS, consumer=c, user=u)
+            except Token.DoesNotExist:
+                t = Token(token_type=Token.ACCESS, consumer=c, user=u)
 
-        t.is_approved = True
-        t.generate_random_codes()
-        t.verifier = VERIFIER
-        t.save()
-        self.token = t
+            t.is_approved = True
+            t.generate_random_codes()
+            t.verifier = VERIFIER
+            t.save()
+            self.token = t
 
     def test_consumerinfo(self):
 
