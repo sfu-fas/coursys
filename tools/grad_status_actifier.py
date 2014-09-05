ecc052edd4b34482a3332e38209b71a8b61a36bf
Greg Baker <ggbaker@sfu.ca>
2014-09-05 11:06:11 -0700

    polish the grad statusifier, since it might become permanent

--- tools/grad_status_actifier.py
+++ tools/grad_status_actifier.py
@@ -6,9 +6,19 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'courses.settings'
 from grad.models import *
 import datetime
 
-sem = Semester.objects.get(name='1147')
-statuses = GradStatus.objects.filter(student__program__unit__label='CMPT', start__name='1147', status='CONF', student__current_status='CONF', hidden=False)
+semestername = '1147'
+
+sem = Semester.objects.get(name=semestername)
+statuses = GradStatus.objects\
+        .filter(student__program__unit__label='CMPT', start__name=semestername,
+        status='CONF', student__current_status='CONF', hidden=False)\
+        .select_related('student')
 students = set(s.student for s in statuses)
+newer_status_students = set(s.student for s in GradStatus.objects
+        .filter(student__program__unit__label='CMPT', student__in=students, start__name__gt=semestername)
+        .select_related('student'))
+students -= newer_status_students
+
 for gs in students:
     print gs
     s = GradStatus(student=gs, status='ACTI', start=sem, start_date=datetime.date(2014, 9, 2))
