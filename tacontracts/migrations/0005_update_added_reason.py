213398c346db06991dec272ff93f7dbfc1e53bdb
Greg Baker <ggbaker@sfu.ca>
2014-10-01 13:15:58 -0700

    add a data migration for new added_reason

---
+++ tacontracts/migrations/0005_update_added_reason.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        Member = orm['coredata.Member']
+        for crs in orm.TACourse.objects.all():
+            # for each course being TAed as a result of a contract here...
+            p = crs.contract.person
+            o = crs.course
+            members = Member.objects.filter(person=p, offering=o, role='TA', added_reason='CTA')
+            if members:
+                # if there is a Member, change its added_reason
+                m = members[0]
+                m.added_reason = 'TAC'
+                m.save()
+
+    def backwards(self, orm):
+        Member = orm['coredata.Member']
+        for m in Member.objects.filter(role='TA', added_reason='TAC'):
+            m.added_reason = 'CTA'
+            m.save()
+
+    models = {
+        u'coredata.course': {
+            'Meta': {'ordering': "('subject', 'number')", 'unique_together': "(('subject', 'number'),)", 'object_name': 'Course'},
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '4', 'db_index': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': 'None', 'unique_with': '()'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '4', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        },
+        u'coredata.courseoffering': {
+            'Meta': {'ordering': "['-semester', 'subject', 'number', 'section']", 'unique_together': "(('semester', 'subject', 'number', 'section'), ('semester', 'crse_id', 'section'), ('semester', 'class_nbr'))", 'object_name': 'CourseOffering'},
+            'campus': ('django.db.models.fields.CharField', [], {'max_length': '5', 'db_index': 'True'}),
+            'class_nbr': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'component': ('django.db.models.fields.CharField', [], {'max_length': '3', 'db_index': 'True'}),
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Course']"}),
+            'crse_id': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'db_index': 'True'}),
+            'enrl_cap': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'enrl_tot': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'flags': ('django.db.models.fields.BigIntegerField', [], {'default': '0'}),
+            'graded': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'instr_mode': ('django.db.models.fields.CharField', [], {'default': "'P'", 'max_length': '2', 'db_index': 'True'}),
+            'members': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'member'", 'symmetrical': 'False', 'through': u"orm['coredata.Member']", 'to': u"orm['coredata.Person']"}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '4', 'db_index': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Unit']", 'null': 'True'}),
+            'section': ('django.db.models.fields.CharField', [], {'max_length': '4', 'db_index': 'True'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Semester']"}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': 'None', 'unique_with': '()'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '4', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '30', 'db_index': 'True'}),
+            'units': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'wait_tot': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        u'coredata.member': {
+            'Meta': {'ordering': "['offering', 'person']", 'object_name': 'Member'},
+            'added_reason': ('django.db.models.fields.CharField', [], {'max_length': '4', 'db_index': 'True'}),
+            'career': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            'credits': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'labtut_section': ('django.db.models.fields.CharField', [], {'max_length': '4', 'null': 'True', 'blank': 'True'}),
+            'offering': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.CourseOffering']"}),
+            'official_grade': ('django.db.models.fields.CharField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'person'", 'to': u"orm['coredata.Person']"}),
+            'role': ('django.db.models.fields.CharField', [], {'max_length': '4'})
+        },
+        u'coredata.person': {
+            'Meta': {'ordering': "['last_name', 'first_name', 'userid']", 'object_name': 'Person'},
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            'emplid': ('django.db.models.fields.PositiveIntegerField', [], {'unique': 'True', 'db_index': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'middle_name': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'pref_first_name': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'temporary': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '4', 'null': 'True', 'blank': 'True'}),
+            'userid': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '8', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'coredata.semester': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Semester'},
+            'end': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '4', 'db_index': 'True'}),
+            'start': ('django.db.models.fields.DateField', [], {})
+        },
+        u'coredata.unit': {
+            'Meta': {'ordering': "['label']", 'object_name': 'Unit'},
+            'acad_org': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '10', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '4', 'db_index': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Unit']", 'null': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': 'None', 'unique_with': '()'})
+        },
+        u'dashboard.newsitem': {
+            'Meta': {'object_name': 'NewsItem'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'author'", 'null': 'True', 'to': u"orm['coredata.Person']"}),
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.CourseOffering']", 'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'published': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'read': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'source_app': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'user'", 'to': u"orm['coredata.Person']"})
+        },
+        u'ra.account': {
+            'Meta': {'ordering': "['account_number']", 'object_name': 'Account'},
+            'account_number': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'hidden': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'position_number': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': 'None', 'unique_with': '()'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Unit']"})
+        },
+        u'tacontracts.emailreceipt': {
+            'Meta': {'object_name': 'EmailReceipt'},
+            'content': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['dashboard.NewsItem']"}),
+            'contract': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'email_receipt'", 'to': u"orm['tacontracts.TAContract']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'tacontracts.hiringsemester': {
+            'Meta': {'unique_together': "(('semester', 'unit'),)", 'object_name': 'HiringSemester'},
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            'deadline_for_acceptance': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pay_end': ('django.db.models.fields.DateField', [], {}),
+            'pay_start': ('django.db.models.fields.DateField', [], {}),
+            'payperiods': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Semester']"}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Unit']"})
+        },
+        u'tacontracts.tacategory': {
+            'Meta': {'object_name': 'TACategory'},
+            'account': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ra.Account']"}),
+            'bu_lab_bonus': ('django.db.models.fields.DecimalField', [], {'default': "'0.17'", 'max_digits': '8', 'decimal_places': '2'}),
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 10, 1, 0, 0)'}),
+            'hidden': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hiring_semester': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tacontracts.HiringSemester']"}),
+            'holiday_hours_per_bu': ('django.db.models.fields.DecimalField', [], {'default': "'1.1'", 'max_digits': '4', 'decimal_places': '2'}),
+            'hours_per_bu': ('django.db.models.fields.DecimalField', [], {'default': "'42'", 'max_digits': '6', 'decimal_places': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pay_per_bu': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'scholarship_per_bu': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '2'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': 'None', 'unique_with': '()'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'tacontracts.tacontract': {
+            'Meta': {'object_name': 'TAContract'},
+            'accepted_by_student': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'appointment': ('django.db.models.fields.CharField', [], {'default': "'INIT'", 'max_length': '4'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'contract'", 'to': u"orm['tacontracts.TACategory']"}),
+            'comments': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'conditional_appointment': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'created_by': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'deadline_for_acceptance': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pay_end': ('django.db.models.fields.DateField', [], {}),
+            'pay_start': ('django.db.models.fields.DateField', [], {}),
+            'payperiods': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['coredata.Person']"}),
+            'sin': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': 'None', 'unique_with': '()'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'NEW'", 'max_length': '4'}),
+            'tssu_appointment': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        u'tacontracts.tacourse': {
+            'Meta': {'unique_together': "(('contract', 'course'),)", 'object_name': 'TACourse'},
+            'bu': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2'}),
+            'config': ('courselib.json_fields.JSONField', [], {'default': '{}'}),
+            'contract': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'course'", 'to': u"orm['tacontracts.TAContract']"}),
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': u"orm['coredata.CourseOffering']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'labtut': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'member': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tacourse'", 'null': 'True', 'to': u"orm['coredata.Member']"}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique_with': '()', 'max_length': '50', 'populate_from': 'None'})
+        }
+    }
+
+    complete_apps = ['tacontracts']
+    symmetrical = True
