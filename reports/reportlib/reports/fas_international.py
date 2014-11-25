b61c6fa4fd72f048a1c6202200349b984fc2c4ec
Greg Baker <ggbaker@sfu.ca>
2014-11-24 17:21:14 -0800

    international query: everybody this year

--- reports/reportlib/reports/fas_international.py
+++ reports/reportlib/reports/fas_international.py
@@ -10,14 +10,25 @@ class InternationalGPAReport(Report):
     description = "GPA for FAS students with visa/citizenship"
 
     def run(self):
-        current_semester = Semester.current().previous_semester()
         cmpt_progs, eng_progs = get_fas_programs()
         fas_progs = cmpt_progs + eng_progs
 
-        students = InternationalGPAQuery({
-            'acad_progs': fas_progs,
-            'strm': current_semester.name,
-        }).result()
+        # collect student data for every semester in the last year.
+        # Do it here because I'm not smart enough to do it in SQL.
+        semester = Semester.current()
+        last_yr = [semester.offset_name(-2), semester.offset_name(-1), semester.name]
+        student_data = {}
+        for strm in last_yr:
+            students = InternationalGPAQuery({
+                'acad_progs': fas_progs,
+                'strm': strm,
+            }).result()
+
+            for r in students.rows:
+                emplid = r[0]
+                student_data[emplid] = r
+
+        students.rows = student_data.values()
 
         visits = AdvisorVisits(unit_slugs=['apsc']).result()
         notes = AdvisorNotes(unit_slugs=['apsc']).result()
