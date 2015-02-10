f3038689e632386d0fa233dd16b6dbb6674264f8
Greg Baker <ggbaker@sfu.ca>
2015-02-10 12:59:05 -0800

    beta (without files) of CRIM impoter

--- advisornotes/management/commands/crim_import.py
+++ advisornotes/management/commands/crim_import.py
@@ -1,10 +1,13 @@
 from django.core.management.base import BaseCommand
+from django.db import transaction
 import unicodecsv as csv
 import datetime
+import os.path
 
 from advisornotes.models import AdvisorNote
 from coredata.models import Person, Unit
 from coredata.queries import add_person
+from courselib.text import normalize_newlines
 
 unit = Unit.objects.get(slug='crim')
 
@@ -12,45 +15,72 @@ class Command(BaseCommand):
     help = 'Import CSV advising data from CRIM.'
     args = '<advisor_userid> <csv_data>'
 
-    def import_note(self, advisor, row):
-        emplid = row[4]
-        fn_transcript = row[3]
-        fn_1 = row[6]
-        fn_2 = row[7]
-        fn_3 = row[9]
-        email = row[10]
-        date_str = row[12]
+    def import_note(self, advisor, fn, i, row):
+        emplid = row['Student ID']
+        #fn_transcript = row['Transcript']
+        #fn_1 = row[6]
+        #fn_2 = row[7]
+        #fn_3 = row[9]
+        date_str = row['Date Modified']
+        notes = normalize_newlines(row['Notes'])
 
-        if not emplid:
+        # fix mis-typed emplids we found
+        # Lindsay
+        if emplid == '960022098':
+            emplid = '963022098'
+        elif emplid == '30108409':
+            emplid = '301078409'
+        elif emplid == '30115964':
+            emplid = '301115964'
+        elif emplid == '30117882':
+            emplid = '301178882'
+        # Michael Sean
+        elif emplid == '30105659':
+            emplid = '301040985' # ?
+        # Dijana
+        elif emplid == '30120965':
+            emplid = '301202965'
+
+
+        if not emplid or emplid == '0':
+            print 'No emplid on row %i' % (i+2)
             return
 
         p = add_person(emplid)
         if not p:
+            print "Can't find person on row %i (emplid %s)" % (i+2, emplid)
             return
 
-        created = datetime.datetime.strptime(date_str, '%m-%d-%Y')
+        try:
+            date = datetime.datetime.strptime(date_str, '%m-%d-%Y').date()
+        except ValueError:
+            date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+        created = datetime.datetime.combine(date, datetime.time(hour=12, minute=0))
 
-        note = AdvisorNote(student=p, advisor=advisor, created_at=created, unit=unit)
-        print note
+        key = '%s-%i' % (fn, i)
+
+        # look for previously-imported version of this note, so we're roughly idempotent
+        oldnotes = AdvisorNote.objects.filter(student=p, advisor=advisor, created_at=created, unit=unit)
+        oldnotes = [n for n in oldnotes if 'import_key' in n.config and n.config['import_key'] == key]
+
+        if oldnotes:
+            note = oldnotes[0]
+        else:
+            note = AdvisorNote(student=p, advisor=advisor, created_at=created, unit=unit)
+
+        note.text = notes
+        note.config['import_key'] = key
+        note.save()
 
 
     def import_notes(self, advisor_userid, inputfile):
         advisor = Person.objects.get(userid=advisor_userid)
         with open(inputfile, 'rb') as fh:
-            data = csv.reader(fh, encoding='utf-8')
+            data = csv.DictReader(fh)
 
-            headers = data.next()
-            assert headers[1] == 'Student'
-            assert headers[3] == 'Transcript'
-            assert headers[4] == 'Student ID'
-            assert headers[6] == 'Files'
-            assert headers[7] == 'Files2'
-            assert headers[9] == 'Files3'
-            assert headers[10] == 'Email'
-            assert headers[12] == 'Date Modified'
-
-            for row in data:
-                self.import_note(advisor, row)
+            for i, row in enumerate(data):
+                with transaction.atomic():
+                    self.import_note(advisor, os.path.split(inputfile)[1], i, row)
 
 
     def handle(self, *args, **options):
