74d5934d381e9ef266ce11f587c2f214b5dad5cd
Greg Baker <ggbaker@sfu.ca>
2015-02-15 23:37:01 -0800

    CRIm advising note import, with files coming along

--- advisornotes/management/commands/crim_import.py
+++ advisornotes/management/commands/crim_import.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.core.files import File
 import unicodecsv as csv
 import datetime
 import os.path
+import mimetypes
+import warnings
 
 from advisornotes.models import AdvisorNote
 from coredata.models import Person, Unit
@@ -13,16 +16,67 @@ unit = Unit.objects.get(slug='crim')
 
 class Command(BaseCommand):
     help = 'Import CSV advising data from CRIM.'
-    args = '<advisor_userid> <csv_data>'
+    args = '<advisor_userid> <csv_data> <file_base>'
+
+    def get_filepath(self, fm_filename):
+        if not fm_filename:
+            return None
+
+        filename = os.path.split(fm_filename)[1]
+        filepath = os.path.join(self.file_base, filename)
+
+        if os.path.isfile(filepath):
+            return filepath
+        else:
+            warnings.warn("Missing file %s." % (filename), RuntimeWarning)
+
+    def get_advisornote(self, key, person, advisor, created, delete_old_file=False, offset=0):
+        """
+        get_or_create for this usage
+        """
+        created = created + datetime.timedelta(minutes=offset)
+        # look for previously-imported version of this note, so we're roughly idempotent
+        oldnotes = AdvisorNote.objects.filter(student=person, advisor=advisor, created_at=created, unit=unit)
+        oldnotes = [n for n in oldnotes if 'import_key' in n.config and n.config['import_key'] == key]
+
+        if oldnotes:
+            note = oldnotes[0]
+            if delete_old_file and note.file_attachment and os.path.isfile(note.file_attachment.path):
+                # let file be recreated below
+                os.remove(note.file_attachment.path)
+                note.file_attachment = None
+                note.file_mediatype = None
+        else:
+            note = AdvisorNote(student=person, advisor=advisor, created_at=created, unit=unit)
+            note.config['import_key'] = key
+
+        return note, bool(oldnotes)
+
+    def attach_file(self, note, filepath):
+        """
+        Use this filepath as the attachment for this note.
+        """
+        with File(open(filepath, 'rb')) as fh:
+            base = os.path.split(filepath)[1]
+            note.file_attachment.save(base, fh)
+
+        mediatype = mimetypes.guess_type(filepath)[0]
+        note.file_mediatype = mediatype
+
 
     def import_note(self, advisor, fn, i, row):
         emplid = row['Student ID']
-        #fn_transcript = row['Transcript']
-        #fn_1 = row[6]
-        #fn_2 = row[7]
-        #fn_3 = row[9]
         date_str = row['Date Modified']
         notes = normalize_newlines(row['Notes'])
+
+        files = [
+            row.get('Transcript', None),
+            row.get('Files', None),
+            row.get('Files2', None),
+            row.get('Files3', None),
+        ]
+        files = map(self.get_filepath, files)
+        files = filter(bool, files)
 
         # fix mis-typed emplids we found
         # Lindsay
@@ -58,30 +112,36 @@ class Command(BaseCommand):
         created = datetime.datetime.combine(date, datetime.time(hour=12, minute=0))
 
         key = '%s-%i' % (fn, i)
+        note, _ = self.get_advisornote(key, p, advisor, created, delete_old_file=True)
 
-        # look for previously-imported version of this note, so we're roughly idempotent
-        oldnotes = AdvisorNote.objects.filter(student=p, advisor=advisor, created_at=created, unit=unit)
-        oldnotes = [n for n in oldnotes if 'import_key' in n.config and n.config['import_key'] == key]
+        print emplid, files
+        if files:
+            path = files[0]
+            self.attach_file(note, path)
 
-        if oldnotes:
-            note = oldnotes[0]
-        else:
-            note = AdvisorNote(student=p, advisor=advisor, created_at=created, unit=unit)
+            for j, path in enumerate(files[1:]):
+                # these get stashed in accompanying notes
+                k = key + '-auxfile-' + str(i)
+                n, _ = self.get_advisornote(k, p, advisor, created, delete_old_file=True, offset=(j+1))
+                n.text = '[Additional file for previous note.]'
+                self.attach_file(n, path)
+                n.save()
 
         note.text = notes
-        note.config['import_key'] = key
         note.save()
 
 
-    def import_notes(self, advisor_userid, inputfile):
+    def import_notes(self, advisor_userid, inputfile, file_base):
+        self.file_base = file_base
         advisor = Person.objects.get(userid=advisor_userid)
         with open(inputfile, 'rb') as fh:
             data = csv.DictReader(fh)
+            fn = os.path.split(inputfile)[1]
 
             for i, row in enumerate(data):
                 with transaction.atomic():
-                    self.import_note(advisor, os.path.split(inputfile)[1], i, row)
+                    self.import_note(advisor, fn, i, row)
 
 
     def handle(self, *args, **options):
-        self.import_notes(args[0], args[1])
+        self.import_notes(args[0], args[1], args[2])
