4fa7b7fd323e084dc8ff53a9565d435ef23be36e
Greg Baker <ggbaker@sfu.ca>
2015-03-09 14:58:24 -0700

    allow serializing multiple grad programs

--- grad/management/commands/grad_serialize.py
+++ grad/management/commands/grad_serialize.py
@@ -7,28 +7,29 @@ from grad.models import GradProgram, GradStudent, GradProgramHistory, Supervisor
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        unit_slug = args[0]
-        unit = Unit.objects.get(slug=unit_slug)
+        unit_slugs = args
+        units = Unit.objects.filter(slug__in=unit_slugs)
 
-        objs = [unit]
-
-        objs.extend(unit.super_units())
+        objs = list(units)
         objs.extend(Semester.objects.all())
-        objs.extend(GradProgram.objects.filter(unit=unit))
-        gss = GradStudent.objects.filter(program__unit=unit).select_related('person')
-        objs.extend(gss)
-        objs.extend(GradProgramHistory.objects.filter(program__unit=unit))
-        supervs = Supervisor.objects.filter(student__program__unit=unit).select_related('supervisor')
-        objs.extend(supervs)
-        objs.extend(GradStatus.objects.filter(student__program__unit=unit))
-        objs.extend(ProgressReport.objects.filter(student__program=unit))
 
-        people = set(gs.person for gs in gss) \
-                 | set(s.supervisor for s in supervs if s.supervisor) \
-                 | set([Person.objects.get(userid='ggbaker')])
+        for unit in units:
+            objs.extend(unit.super_units())
+            objs.extend(GradProgram.objects.filter(unit=unit))
+            gss = GradStudent.objects.filter(program__unit=unit).select_related('person')
+            objs.extend(gss)
+            objs.extend(GradProgramHistory.objects.filter(program__unit=unit))
+            supervs = Supervisor.objects.filter(student__program__unit=unit).select_related('supervisor')
+            objs.extend(supervs)
+            objs.extend(GradStatus.objects.filter(student__program__unit=unit))
+            objs.extend(ProgressReport.objects.filter(student__program=unit))
 
-        objs.extend(people)
-        objs.extend(Role.objects.filter(person__userid='ggbaker', unit=unit))
+            people = set(gs.person for gs in gss) \
+                     | set(s.supervisor for s in supervs if s.supervisor) \
+                     | set([Person.objects.get(userid='ggbaker')])
+
+            objs.extend(people)
+            objs.extend(Role.objects.filter(person__userid='ggbaker', unit=unit))
 
         data = serializers.serialize("json", objs, sort_keys=True, indent=1)
         print data
