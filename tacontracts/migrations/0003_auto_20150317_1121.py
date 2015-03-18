e4316f690b6e699cfae1c55667f8d4dea643b298
Phil Boutros <pba7@sfu.ca>
2015-03-18 12:20:23 -0700

    Missing migrations

---
+++ tacontracts/migrations/0003_auto_20150317_1121.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tacontracts', '0002_tacategory'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='tacategory',
+            name='created',
+            field=models.DateTimeField(default=datetime.datetime.now, editable=False),
+            preserve_default=True,
+        ),
+    ]
---
+++ visas/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+import courselib.json_fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('coredata', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Visa',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('start_date', models.DateField(default=datetime.datetime(2015, 3, 17, 11, 20, 57, 882785), verbose_name=b'Star Date')),
+                ('end_date', models.DateField(verbose_name=b'End Date')),
+                ('config', courselib.json_fields.JSONField(default={}, editable=False)),
+                ('hidden', models.BooleanField(default=False, editable=False)),
+                ('person', models.ForeignKey(to='coredata.Person')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
