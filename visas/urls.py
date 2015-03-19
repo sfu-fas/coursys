fc9c32e426914be3584434882e6c2e23d27d0c40
Phil Boutros <pba7@sfu.ca>
2015-03-19 11:19:08 -0700

    Simple editing code

--- visas/urls.py
+++ visas/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 visas_pattern = [ # prefix /visas/
     url(r'^$', 'visas.views.list_all_visas'),
     url(r'^new_visa$', 'visas.views.new_visa'),
+    url(r'^(?P<visa_id>\d+)/edit_visa/$', 'visas.views.edit_visa'),
 ]
--- visas/views.py
+++ visas/views.py
@@ -1,7 +1,7 @@
 from courselib.auth import requires_global_role
 from .models import Visa
 from .forms import VisaForm
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 
@@ -30,7 +30,27 @@ def new_visa(request):
 
     return render(request, 'visas/new_visa.html', {'form': form})
 
+@requires_global_role("SYSA")
+def edit_visa(request, visa_id):
+    visa = get_object_or_404(Visa, pk=visa_id)
+    if request.method == 'POST':
+        form = VisaForm(request.POST, instance=visa)
+        if form.is_valid():
+            visa = form.save(commit=False)
+            visa.save()
+            messages.add_message(request,
+                                 messages.SUCCESS,
+                                 u'Visa was successfully modified.'
+                                 )
 
+            return HttpResponseRedirect(reverse('visas.views.list_all_visas'))
+    else:
+        form = VisaForm(instance=visa)
+
+    return render(request, 'visas/edit_visa.html', {'form': form, 'visa_id': visa_id})
+
+
+    
 
 
 
