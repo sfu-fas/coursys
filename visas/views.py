a416e4ac76f8640ef610170fa6384d2be03e6c35
Phil Boutros <pba7@sfu.ca>
2015-05-05 14:19:32 -0700

    Added full visa list download link

--- visas/views.py
+++ visas/views.py
@@ -1,19 +1,21 @@
-from courselib.auth import requires_global_role
+from courselib.auth import requires_global_role, requires_role
 from .models import Visa
 from .forms import VisaForm
-from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 from log.models import LogEntry
+from datetime import datetime
+import unicodecsv as csv
 
 
-@requires_global_role("SYSA")
+@requires_role(["TAAD", "GRAD", "ADMN", "GRPD"])
 def list_all_visas(request):
     context = {'visa_list': Visa.objects.visible}
     return render(request, 'visas/view_visas.html', context)
 
 
-@requires_global_role("SYSA")
+@requires_role(["TAAD", "GRAD", "ADMN", "GRPD"])
 def new_visa(request):
     if request.method == 'POST':
         form = VisaForm(request.POST)
@@ -36,7 +38,8 @@ def new_visa(request):
 
     return render(request, 'visas/new_visa.html', {'form': form})
 
-@requires_global_role("SYSA")
+
+@requires_role(["TAAD", "GRAD", "ADMN", "GRPD"])
 def edit_visa(request, visa_id):
     visa = get_object_or_404(Visa, pk=visa_id)
     if request.method == 'POST':
@@ -61,7 +64,7 @@ def edit_visa(request, visa_id):
     return render(request, 'visas/edit_visa.html', {'form': form, 'visa_id': visa_id})
 
 
-@requires_global_role("SYSA")
+@requires_role(["TAAD", "GRAD", "ADMN", "GRPD"])
 def delete_visa(request, visa_id):
     visa = get_object_or_404(Visa, pk=visa_id)
     messages.success(request, 'Hid visa for %s' % (visa.person.name()))
@@ -76,4 +79,19 @@ def delete_visa(request, visa_id):
     return HttpResponseRedirect(reverse(list_all_visas))
 
 
+@requires_role(["TAAD", "GRAD", "ADMN", "GRPD"])
+def download_visas_csv(request):
+    visas = Visa.objects.visible()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'inline; filename="%s.csv"' % datetime.now().strftime('%Y%m%d')
+    writer = csv.writer(response)
+    writer.writerow(['Person', 'Start Date', 'End Date', 'Type', 'Validity'])
+    for v in visas:
+        person = v.person
+        start_date = v.start_date
+        end_date = v.end_date
+        visa_type = v.status
+        validity = v.get_validity()
+        writer.writerow([person, start_date, end_date, visa_type, validity])
 
+    return response
