e15d9b8639f16fcaa169cac55b32741e59f42e3b
Greg Baker <ggbaker@sfu.ca>
2015-06-16 11:06:24 -0700

    allow --nomigrations to speed up tests; update a few more libs

--- courses/settings.py
+++ courses/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = (
     #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test_without_migrations',
     #'south',
     'compressor',
     'haystack',
