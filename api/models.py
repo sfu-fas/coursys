fbdc916bc422506ee0395c65e2a4ecb444853133
Greg Baker <ggbaker@sfu.ca>
2015-06-15 21:22:12 -0700

    fix typo

--- api/models.py
+++ api/models.py
@@ -9,8 +9,8 @@ from time import time
 
 PERMISSION_CHOICES = [
     ('courses', 'View the courses you are enrolled in'),
-    ('grades', 'View the marks you have received in your courses')
-    ('groups', 'Group memberships in your courses')
+    ('grades', 'View the marks you have received in your courses'),
+    ('groups', 'Group memberships in your courses'),
 ]
 PERMISSION_OPTIONS = dict(PERMISSION_CHOICES)
 
