4b92046b8a02220f0d5fd450d8436fc20c5add2d
Greg Baker <ggbaker@sfu.ca>
2015-06-25 11:10:25 -0700

    start of a recipe for a database server

---
+++ machines/chef/cookbooks/db_server/recipes/default.rb
@@ -0,0 +1,5 @@
+package "mysql-server"
+
+# TODO: my.cnf file, possibly from tools.percona.com/members/wizard
+# TODO: user setup: root password and app user
+# TODO: listen remotely, but only for courses.cs
