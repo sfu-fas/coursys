a0935f9869397dc8ae585eacae37e5812d48c1e6
Greg Baker <ggbaker@sfu.ca>
2015-06-25 18:23:51 -0700

    working my.cnf file for db recipe

--- machines/chef/cookbooks/db_server/recipes/default.rb
+++ machines/chef/cookbooks/db_server/recipes/default.rb
@@ -1,5 +1,18 @@
 package "mysql-server"
 
-# TODO: my.cnf file, possibly from tools.percona.com/members/wizard
+cookbook_file "my.cnf" do
+    path "/etc/mysql/my.cnf"
+end
+
+service "mysql" do
+  action :reload
+end
+
+execute "mysql-root-password" do
+    # make sure password isn't empty
+    command "mysqladmin -u root password somepassword"
+    only_if "mysql -uroot" # i.e. only if we can log in with no password
+end
+
 # TODO: user setup: root password and app user
 # TODO: listen remotely, but only for courses.cs
