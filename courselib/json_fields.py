c73a5ac9b74b803efb6d457697e69a86db12a39c
Phil Boutros <pba7@sfu.ca>
2015-10-08 10:29:35 -0700

    Minor cleanup of old code that hasn't been used in 3 years

--- courselib/json_fields.py
+++ courselib/json_fields.py
@@ -1,27 +1,21 @@
 import copy
 
+
 # tool to create convenient getters and setters for .config fields
 def getter_setter(field):
     def getter(self):
         return self.config[field] if field in self.config else copy.copy(self.defaults[field])
+
     def setter(self, val):
         self.config[field] = val
     return getter, setter
 
-# for nested config fields
-def getter_setter_2(field, subfield):
-    def getter(self):
-        return (self.config[field][subfield] 
-                if field in self.config 
-                else copy.copy(self.defaults[field][subfield]))
-    def setter(self, val):
-        self.config[field][subfield] = val
-    return getter, setter
 
 # better version of getter_setter
 def config_property(field, default):
     def getter(self):
         return self.config[field] if field in self.config else copy.copy(default)
+
     def setter(self, val):
         self.config[field] = val
     return property(getter, setter)
