bf733f01a4b110ced1c68d844702b18b604fbeb6
Phil Boutros <pba7@sfu.ca>
2015-11-26 13:30:38 -0800

    Move download link in visa urls for correct matching first before emplid

--- visas/urls.py
+++ visas/urls.py
@@ -4,8 +4,8 @@ from courselib.urlparts import EMPLID_SLUG
 visas_pattern = [ # prefix /visas/
     url(r'^$', 'visas.views.list_all_visas'),
     url(r'^new_visa$', 'visas.views.new_visa'),
-    url(r'^(?P<emplid>[\w\-]+)' + '$', 'visas.views.list_all_visas'),
     url(r'^download', 'visas.views.download_visas_csv'),
+    url(r'^(?P<emplid>[\w\-]+)' + '$', 'visas.views.list_all_visas'),
     url(r'^new_visa/' + '(?P<emplid>[\w\-]+)' + '$', 'visas.views.new_visa'),
     url(r'^(?P<visa_id>\d+)/edit_visa/$', 'visas.views.edit_visa'),
     url(r'^(?P<visa_id>\d+)/delete_visa/$', 'visas.views.delete_visa'),
