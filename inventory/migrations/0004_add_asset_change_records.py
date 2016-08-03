708f15e92e5e85f4e483f711637dd576c3e96b4b
Phil Boutros <pba7@sfu.ca>
2016-08-03 14:32:04 -0700

    Add records to track changes in asset quantities

---
+++ inventory/migrations/0004_add_asset_change_records.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import autoslug.fields
+import courselib.json_fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('outreach', '0001_initial'),
+        ('coredata', '0014_auto_20160623_1509'),
+        ('inventory', '0003_auto_20160728_1402'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AssetChangeRecord',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('qty', models.IntegerField(help_text=b"The change in quantity.  For removal of item, make it a negative number. For adding items, make it a positive.  e.g. '-2' if someone removed two ofthis item for something", verbose_name=b'Quantity adjustment')),
+                ('date', models.DateField(null=True, blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('last_modified', models.DateTimeField(editable=False)),
+                ('hidden', models.BooleanField(default=False, editable=False)),
+                ('saved_by_userid', models.CharField(max_length=8, editable=False)),
+                ('config', courselib.json_fields.JSONField(default=dict)),
+                ('slug', autoslug.fields.AutoSlugField(populate_from=b'autoslug', unique=True, editable=False)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='asset',
+            name='config',
+            field=courselib.json_fields.JSONField(default=dict),
+        ),
+        migrations.AddField(
+            model_name='asset',
+            name='last_order_date',
+            field=models.DateField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='asset',
+            name='min_vendor_qty',
+            field=models.PositiveIntegerField(help_text=b'The minimum quantity the vendor will let us order', null=True, verbose_name=b'Minimum vendor order quantity', blank=True),
+        ),
+        migrations.AddField(
+            model_name='assetchangerecord',
+            name='asset',
+            field=models.ForeignKey(to='inventory.Asset'),
+        ),
+        migrations.AddField(
+            model_name='assetchangerecord',
+            name='event',
+            field=models.ForeignKey(help_text=b'The event it was for, if any', to='outreach.OutreachEvent'),
+        ),
+        migrations.AddField(
+            model_name='assetchangerecord',
+            name='person',
+            field=models.ForeignKey(to='coredata.Person'),
+        ),
+    ]
--- inventory/models.py
+++ inventory/models.py
@@ -3,12 +3,14 @@ A module written for inventory control of any type of asset we may want.
 """
 
 
-from coredata.models import Unit
+from coredata.models import Unit, Person
+from outreach.models import OutreachEvent
 from autoslug import AutoSlugField
 from django.db import models
 from django.utils import timezone
 from django.db.models import Q
 from courselib.slugs import make_slug
+from courselib.json_fields import JSONField
 
 CATEGORY_CHOICES = {
     ('SWAG', 'Swag'),
@@ -40,6 +42,9 @@ class Asset(models.Model):
                                           help_text="The minimum quantity that should be in stock before having to "
                                                     "re-order")
     qty_ordered = models.PositiveIntegerField("Quantity on order", blank=True, null=True)
+    min_vendor_qty = models.PositiveIntegerField("Minimum vendor order quantity", blank=True, null=True,
+                                                 help_text="The minimum quantity the vendor will let us order")
+    last_order_date = models.DateField(null=True, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     category = models.CharField(max_length=4, choices=CATEGORY_CHOICES, null=True, blank=True, default='GEN')
     location = models.CharField(max_length=150, null=True, blank=True)
@@ -47,6 +52,8 @@ class Asset(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     last_modified = models.DateTimeField(editable=False, blank=False, null=False)
     hidden = models.BooleanField(default=False, null=False, blank=False, editable=False)
+    # In case we forgot something, this will make it easier to add something in the future without a migration.
+    config = JSONField(null=False, blank=False, default=dict)
 
     objects = AssetQuerySet.as_manager()
 
@@ -78,3 +85,51 @@ class Asset(models.Model):
 
     def in_stock(self):
         return self.quantity is not None and self.min_qty is not None and self.quantity > self.min_qty
+
+
+class AssetChangeRecordQuerySet(models.QuerySet):
+    """
+    Only see visible items, in this case also limited by a given asset.
+    """
+    def visible(self, asset):
+        return self.filter(hidden=False, asset=asset)
+
+
+class AssetChangeRecord(models.Model):
+    asset = models.ForeignKey(Asset, null=False, blank=False)
+    person = models.ForeignKey(Person, null=False, blank=False)
+    qty = models.IntegerField("Quantity adjustment", null=False, blank=False,
+                              help_text="The change in quantity.  For removal of item, make it a negative number. "
+                                        "For adding items, make it a positive.  e.g. '-2' if someone removed two of"
+                                        "this item for something")
+    event = models.ForeignKey(OutreachEvent, null=False, blank=False, help_text="The event it was for, if any")
+    date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified = models.DateTimeField(editable=False, blank=False, null=False)
+    hidden = models.BooleanField(default=False, null=False, blank=False, editable=False)
+    saved_by_userid = models.CharField(max_length=8, blank=False, null=False, editable=False)
+    # In case we forgot something, this will make it easier to add something in the future without a migration.
+    config = JSONField(null=False, blank=False, default=dict)
+
+    def autoslug(self):
+        if self.qty > 0:
+            change_string=" added "
+        else:
+            change_string=" removed "
+        return make_slug(self.person + change_string + self.qty + ' ' + self.asset)
+
+    slug = AutoSlugField(populate_from='autoslug', null=False, editable=False, unique=True)
+
+    objects = AssetChangeRecordQuerySet.as_manager()
+
+    def save(self, user, *args, **kwargs):
+        self.last_modified = timezone.now()
+        self.saved_by_userid = user
+        super(Asset, self).save(*args, **kwargs)
+
+    def delete(self, user):
+        """
+        Like most of our objects, never actually delete them, just hide them.
+        """
+        self.hidden = True
+        self.save(user)
