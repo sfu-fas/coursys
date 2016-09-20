4bf5eb03f340863472e7c9d4db73750ce5be9067
Phil Boutros <pba7@sfu.ca>
2016-09-20 11:34:57 -0700

    Change ID to the current one of ENSC in prod for migration

--- visas/migrations/0006_add_unit_to_visas.py
+++ visas/migrations/0006_add_unit_to_visas.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='visa',
             name='unit',
-            field=models.ForeignKey(default=3, to='coredata.Unit'),
+            field=models.ForeignKey(default=34, to='coredata.Unit'),
             preserve_default=False,
         ),
     ]
