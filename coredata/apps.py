21fc3f5c4e90ff8c65fedfe43ea95221c52b30b4
Greg Baker <ggbaker@sfu.ca>
2017-01-18 21:20:52 -0800

    add check to warn about broken sqlite version

--- coredata/apps.py
+++ coredata/apps.py
@@ -2,3 +2,19 @@ from django.apps import AppConfig
 
 class CoredataConfig(AppConfig):
     name = 'coredata'
+
+
+from django.core.checks import Warning, register
+@register()
+def sqlite_check(app_configs, **kwargs):
+    errors = []
+    import sqlite3
+    if sqlite3.sqlite_version_info < (3, 12):
+        errors.append(
+            Warning(
+                'SQLite version problem',
+                hint='A bug is sqlite version 3.11.x causes a segfault in our tests. Upgrading to >=3.14 is suggested. This is only a warning because many things still work. Just not the tests.',
+                id='coredata.E001',
+            )
+        )
+    return errors
