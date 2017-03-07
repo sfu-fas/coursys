b7ff1fec9af85ece87ffd11823c9d2a6c375cfcc
Greg Baker <ggbaker@sfu.ca>
2017-03-06 18:06:13 -0800

    start some A/B testing on the site search

--- dashboard/context.py
+++ dashboard/context.py
@@ -3,24 +3,30 @@ from coredata.models import Member
 from cache_utils.decorators import cached
 from courselib.branding import product_name, help_email
 
-@cached(60)
-def context_memberships(userid):
-    if userid:
-        return Member.get_memberships(userid)[0]
-    else:
-        return []
+
+@cached(3600)
+def is_instr_ta(userid):
+    if not userid:
+        return False
+    members = Member.objects.filter(role__in=['INST', 'TA'])
+    return members.exists()
+
 
 def media(request):
     """
     Add context things that we need
     """
+    # A/B testing: half of instructors and TAs see a different search box
+    instr_ta = is_instr_ta(request.user.username)
+    instr_ta_ab = instr_ta and request.user.is_authenticated and request.user.id % 2 == 0
     # GRAD_DATE(TIME?)_FORMAT for the grad/ra/ta apps
     return {'GRAD_DATE_FORMAT': settings.GRAD_DATE_FORMAT,
             'GRAD_DATETIME_FORMAT': settings.GRAD_DATETIME_FORMAT,
             'LOGOUT_URL': settings.LOGOUT_URL,
             'LOGIN_URL': settings.LOGIN_URL,
             'STATIC_URL': settings.STATIC_URL,
-            'memberships': context_memberships(request.user.username),
+            'is_instr_ta': instr_ta,
+            'instr_ta_ab': instr_ta_ab,
             'request_path': request.path,
             'CourSys': product_name(request),
             'help_email': help_email(request),
