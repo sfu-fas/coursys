51cd98db824005a71bacf7cc886a9b72a73510d0
Phil Boutros <pba7@sfu.ca>
2017-07-13 13:46:10 -0700

    Beginning of work on space management

---
+++ space/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from coredata.models import Unit
+from autoslug import AutoSlugField
+from courselib.slugs import make_slug
+from coredata.models import CAMPUS_CHOICES
+
+
+BUILDING_CHOICES = (
+    ('ASB', 'Applied Sciences Building'),
+    ('TASC1', 'TASC 1'),
+    ('SRY', 'Surrey'),
+    ('SEE', 'SEE Building'),
+    ('PTECH', 'Powertech'),
+    ('BLARD', 'Ballard'),
+    ('CC1', 'CC1'),
+    ('NTECH', 'NEUROTECH'),
+    ('SMH', 'SMH')
+)
+
+OWN_CHOICES = (
+    ('OWN', 'SFU Owned'),
+    ('LEASE', 'Leased')
+)
+
+INFRASTRUCTURE_CHOICES = (
+    ('WET', 'Wet Lab'),
+    ('DRY', 'Dry Lab'),
+    ('HPC', 'High-Performance Computing'),
+    ('STD', 'Standard')
+)
+
+CATEGORY_CHOICES = (
+    ('STAFF', 'Staff'),
+    ('FAC', 'Faculty'),
+    ('PDOC', 'Post-Doc'),
+    ('VISIT', 'Visitor'),
+    ('SESS', 'Sessional'),
+    ('LTERM', 'Limited-Term'),
+    ('TA', 'TA'),
+    ('GRAD', 'Grad Student'),
+    ('URA', 'URA')
+)
+
+
+class LocationManager(models.QuerySet):
+    def visible(self, units):
+        """
+        Only see visible items, in this case also limited by accessible units.
+        """
+        return self.filter(hidden=False, unit__in=units)
+
+
+class Location(models.Model):
+    unit = models.ForeignKey(Unit, null=False)
+    campus = models.CharField(max_length=5, choices=CAMPUS_CHOICES, null=True, blank=True)
+    building = models.CharField(max_length=5, choices=BUILDING_CHOICES, null=True, blank=True)
+    floor = models.PositiveIntegerField(null=True, blank=True)
+    room_number = models.CharField(max_length=25, null=True, blank=True)
+    square_meters = models.DecimalField(max_digits=8, decimal_places=2)
+    infrastructure = models.CharField(max_length=3, choices=INFRASTRUCTURE_CHOICES, null=True, blank=True)
+    room_capacity = models.PositiveIntegerField(null=True, blank=True)
+    category = models.CharField(max_length=5, choices=CATEGORY_CHOICES, null=True, blank=True)
+    occupancy_count = models.PositiveIntegerField(null=True, blank=True)
+    own_or_lease = models.CharField("SFU Owned or Leased", max_length=5, choices=OWN_CHOICES, null=True, blank=True)
+    comments = models.CharField(max_length=400, null=True, blank=True)
+    hidden = models.BooleanField(default=False, null=False, blank=False, editable=False)
+
+    def autoslug(self):
+        return make_slug(self.unit.slug + '-' + self.campus + '-' + self.building + '-' + str(self.floor) + '-' +
+                         self.room_number)
+    slug = AutoSlugField(populate_from='autoslug', null=False, editable=False, unique=True)
+
+    def __unicode__(self):
+        return u"%s - %s - %s - %s - %s" % (self.unit.label, self.campus, self.building, str(self.floor),
+                                            self.room_number)
