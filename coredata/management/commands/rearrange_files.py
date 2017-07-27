2b78058bd39eab61e6c9b3184153c0b4162eb012
Greg Baker <ggbaker@sfu.ca>
2017-07-27 12:06:10 -0700

    beta file rearranger

--- coredata/management/commands/rearrange_files.py
+++ coredata/management/commands/rearrange_files.py
@@ -1,11 +1,39 @@
 from django.core.management.base import BaseCommand
 from django.apps import apps
 from django.db import models
-import os
+import os, re
+from threading import Thread
 
 
-def filename(path):
-    return os.path.split(path)[1]
+uuid_re = re.compile(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
+
+
+def move_file(model, storage, field_name, field, o):
+    fld = getattr(o, field_name)
+
+    old_loc = fld.name
+    new_loc = field.upload_to(o, os.path.split(old_loc)[1])
+    old_loc_full = storage.path(old_loc)
+    new_loc_full = storage.path(new_loc)
+    old_dir = os.path.split(old_loc_full)[0]
+    new_dir = os.path.split(new_loc_full)[0]
+
+    if old_loc == new_loc or uuid_re.search(old_loc):
+        # already in place or contains a UUID (i.e. is probably new format already)
+        return
+
+    # link/move/unlink ensures that this can be safely interrupted
+    print('%s > %s' % (old_loc, new_loc))
+    os.makedirs(new_dir)
+    os.link(old_loc_full, new_loc_full)
+    up = {field_name: new_loc}
+    model.objects.filter(pk=o.pk).update(**up)  # avoid any .save() logic
+    os.remove(old_loc_full)
+
+    # clean up newly-empty directories
+    while not os.listdir(old_dir):
+        os.rmdir(old_dir)
+        old_dir = os.path.split(old_dir)[0]
 
 
 class Command(BaseCommand):
@@ -15,7 +43,6 @@ class Command(BaseCommand):
         parser.add_argument('field', type=str, help='the FileField in the model')
 
     def handle(self, *args, **options):
-        raise NotImplementedError("This needs more testing.")
         app_name = options['app']
         model_name = options['model']
         field_name = options['field']
@@ -27,18 +54,7 @@ class Command(BaseCommand):
         storage = field.storage
 
         for o in model.objects.all():
-            fld = getattr(o, field_name)
-            old_loc = fld.file.name
-            new_loc = field.upload_to(o, filename(old_loc))
-            new_loc_full = storage.path(new_loc)
-            newdir, newfile = os.path.split(new_loc_full)
-            if old_loc == new_loc:
-                continue
-
-            # link/move/unlink ensures that this can be safely interrupted
-            os.makedirs(newdir)
-            os.link(old_loc, new_loc_full)
-            up = {field_name: new_loc}
-            model.objects.filter(pk=o.pk).update(**up) # avoid any .save() logic
-            os.remove(old_loc)
-
+            # threads aren't usually interrupted: https://stackoverflow.com/a/842567/6871666
+            t = Thread(target=move_file, args=(model, storage, field_name, field, o))
+            t.start()
+            t.join()
