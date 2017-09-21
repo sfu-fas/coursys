d7ef322280fed9b4d13df852b4ea44a4abdab6ed
Greg Baker <ggbaker@sfu.ca>
2017-09-21 11:44:47 -0700

    enable let's encrypt certs for new domains

--- machines/chef/cookbooks/coursys_true_production/recipes/default.rb
+++ machines/chef/cookbooks/coursys_true_production/recipes/default.rb
@@ -34,7 +34,7 @@ apt_repository 'certbot' do
   key '75BCA694'
   deb_src      false
 end
-package 'python-certbot-nginx'
+#package 'python-certbot-nginx' # currently needs python-acme >=17.0 manually installed https://packages.ubuntu.com/artful/python-acme
 
 # reconfigure NGINX with end-user properties
 cookbook_file "nginx_base.conf" do
