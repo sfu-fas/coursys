0eff2061faa5a0db0c76dce3b185bbffd8d2af44
Phil Boutros <pba7@sfu.ca>
2017-10-04 11:18:07 -0700

    Enforce effective date order in GradStatuses for Financial page

--- grad/views/financials.py
+++ grad/views/financials.py
@@ -101,7 +101,7 @@ def financials(request, grad_slug, style='complete'):
         # grad status        
         status = None
         status_short = None
-        for s in GradStatus.objects.filter(student=grad, hidden=False):
+        for s in GradStatus.objects.filter(student=grad, hidden=False).order_by('start_date'):
             if s.start <= semester and (s.end == None or semester <= s.end) :
                 status = s.get_status_display()
                 status_short = s.get_short_status_display()
