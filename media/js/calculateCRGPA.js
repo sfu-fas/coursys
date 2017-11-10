ef2216130d77a6e426ee586739a2773509ae55dd
Phil Boutros <pba7@sfu.ca>
2017-11-10 12:01:36 -0800

    Tweaks to the JS file for one specific form.

--- media/js/calculateCRGPA.js
+++ media/js/calculateCRGPA.js
@@ -11,8 +11,14 @@ $(document).ready(function() {
     var output = $("#id_10");
     // First thing to do is to disable this input to stop students from changing it.
     output.prop("readonly", true);
-    // Every time we change a dropdown, recalculate the CRGPA
-    $("select").change(function () { calculateCRGPA(); });
+    // The very first time we load, change the second drop-down selected value to the second one.  This will make sense
+    // when looking at disableDuplicates.
+    $("#id_6").val("choice_2")
+    // Every time we change a dropdown, disable duplicates in CMPT cousres and recalculate the CRGPA
+    $("select").change(function () {
+        disableDuplicates();
+        calculateCRGPA();
+    });
     // Run it once so the value is there, just in the ridiculously low possibility that the defaults are correct
     // for some student.
     calculateCRGPA();
@@ -34,6 +40,21 @@ function getCredits(selector) {
 /* The value is already in the drop down, just make sure it"s a float. */
 function getGPAValue(selector) {
     return parseFloat(selector.val());
+}
+
+function disableDuplicates() {
+    // Some CMPT courses are in both the dropdowns.  Every time we change the values, make sure the duplicates are
+    // disabled.
+
+    // Re-enable every option...
+    $("#id_4 option").removeAttr("disabled")
+    // Then find the one that is selected in the other drop-down and disabled that.
+    $("#id_4 option").filter(function() {
+        return $(this).text().trim() == $('#id_6').children(":selected").text().trim();}).attr('disabled','disabled');
+    // Lather, rinse, repeat for the other one.
+    $("#id_6 option").removeAttr("disabled")
+    $("#id_6 option").filter(function() {
+        return $(this).text().trim() == $('#id_4').children(":selected").text().trim();}).attr('disabled','disabled');
 }
 
 function calculateCRGPA() {
