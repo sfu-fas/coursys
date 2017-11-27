4584e8509caf7d831fbf5ef7aa5138a8240200ad
Phil Boutros <pba7@sfu.ca>
2017-11-27 12:08:42 -0800

    Add current booking and grad student count to space management download

--- space/views.py
+++ space/views.py
@@ -7,6 +7,7 @@ from .forms import LocationForm, RoomTypeForm, BookingRecordForm, BookingRecordA
 from courselib.auth import requires_role
 from log.models import LogEntry
 from coredata.models import Unit, Person
+from grad.models import Supervisor
 import datetime
 import unicodecsv as csv
 
@@ -30,13 +31,20 @@ def download_locations(request):
     writer = csv.writer(response)
     writer.writerow(['Unit', 'Campus', 'Building', 'Floor', 'Room Number', 'Square Meters', 'Room Type Description',
                      'Room Type Code', 'COU Code Description', 'Space Factor', 'COU Code Value', 'Infrastructure',
-                     'Room Capacity', 'Category', 'Occupancy', 'Own/Leased', 'Comments'])
+                     'Room Capacity', 'Category', 'Occupancy', 'Own/Leased', 'Comments', 'Current Booking',
+                     'Active Grad Student(s)'])
     for l in locations:
+        booking = l.get_current_booking()
+        grad_count = None
+        if booking:
+            booker = booking.person
+            grad_count = Supervisor.objects.filter(supervisor=booker, removed=False, student__current_status='ACTI').count()
+
         writer.writerow([l.unit.name, l.get_campus_display(), l.get_building_display(), l.floor, l.room_number,
                          l.square_meters, l.room_type.long_description, l.room_type.code,
                          l.room_type.COU_code_description, l.room_type.space_factor, l.room_type.COU_code_value,
                          l.get_infrastructure_display(), l.room_capacity, l.get_category_display(), l.occupancy_count,
-                         l.get_own_or_lease_display(), l.comments])
+                         l.get_own_or_lease_display(), l.comments, booking, grad_count])
     return response
 
 
