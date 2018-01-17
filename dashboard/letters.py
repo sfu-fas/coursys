74d0a409a79d9e9fc5fb18b4a01805d8299360fd
Phil Boutros <pba7@sfu.ca>
2018-01-17 12:38:19 -0800

    Make the filling of position a little smarter

--- dashboard/letters.py
+++ dashboard/letters.py
@@ -588,7 +588,11 @@ class RAForm(SFUMediaMixin):
         self._box_entry(71*mm, 139*mm, 11.5*mm, 6.5*mm, content="%i" % self.ra.project.fund_number)
         self._box_entry(85*mm, 139*mm, 25*mm, 6.5*mm, content=str(self.ra.project.department_code))
         self._box_entry(112*mm, 139*mm, 31*mm, 6.5*mm, content=self.ra.get_program_display())
-        self._box_entry(150*mm, 139*mm, 46*mm, 6.5*mm, content=str(self.ra.account.position_number))
+        if self.ra.account.position_number <= 1:
+            position_number = ''
+        else:
+            position_number = str(self.ra.account.position_number).zfill(9)
+        self._box_entry(150*mm, 139*mm, 46*mm, 6.5*mm, content=position_number)
 
         # dates
         self.c.setFont("Helvetica", 8)
