4a2160df069eb35eb8c6d13915943a23c9b96b9a
Phil Boutros <philb@philb.ca>
2019-03-25 09:34:16 -0700

    Add another offering component to the importer

--- coredata/models.py
+++ coredata/models.py
@@ -864,6 +864,7 @@ COMPONENT_CHOICES = (
         ('CNV', 'CNV'), # converted from SIMON?
         ('OPL', 'Open Lab'), # ???
         ('EXM', 'Exam'),  # First showed up 2017/09/20 with descr "PhD Oral Candidacy Exam"
+        ('CAP', 'Capstone Required'),  # First showed up 2019/03/24.  go.sfu shows those classes as "Capstone Required"
         ('CAN', 'Cancelled')
         )
 COMPONENTS = dict(COMPONENT_CHOICES)
