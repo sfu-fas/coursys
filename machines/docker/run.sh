2be56da3a32f4e18b7f968e3dee500b6f06a8ac6
Greg Baker <ggbaker@sfu.ca>
2019-04-12 13:19:22 -0700

    update docker image generation

--- machines/docker/run.sh
+++ machines/docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo docker run coursys-test-py3
-# sudo docker run -ti coursys-test-py3 /bin/bash
+sudo docker run ggbaker/coursys-test-py3
+# sudo docker run -ti ggbaker/coursys-test-py3 /bin/bash
