453d12e66bca463ae4989518156c5d4cd80ef150
Phil Boutros <philb@philb.ca>
2019-06-10 15:49:55 -0700

    Attempt to stop celery timing out fetching photos

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -2,14 +2,18 @@ from django.conf import settings
 from courselib.celerytasks import task, periodic_task
 from celery.schedules import crontab
 from dashboard.photos import do_photo_fetch, change_photo_password
+from requests.exceptions import RequestException
 
-@task(queue='photo')
+
+@task(queue='photo', autoretry_for=(RequestException,), retry_backoff=True)
 def fetch_photos_task(emplids):
     return do_photo_fetch(emplids)
+
 
 if not settings.USE_CELERY:
     # no celery? Disable.
     fetch_photos_task.delay = lambda emplids: None
+
 
 @periodic_task(run_every=crontab(day_of_month="10,20,30", hour=2, minute=0))
 def photo_password_update_task():
