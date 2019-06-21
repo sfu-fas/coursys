273568ca08cea9f1ef8a1d45cfb74c1045a1178c
Greg Baker <ggbaker@sfu.ca>
2019-06-21 13:45:41 -0700

    purge piwik integration, which is long-dead

--- coredata/panel.py
+++ coredata/panel.py
@@ -232,23 +232,6 @@ def deploy_checks(request=None):
     else:
         passed.append(('Emplid API', 'okay'))
 
-    # Piwik API
-    #if not request:
-    #    failed.append(('Piwik API', "can only check in web frontend with valid request object"))
-    #elif not settings.PIWIK_URL or not settings.PIWIK_TOKEN:
-    #    failed.append(('Piwik API', "not configured in secrets.py"))
-    #else:
-    #    # try to re-log this request in piwik and see what happens
-    #    from piwik_middleware.tracking import PiwikTrackerLogic, urllib_errors
-    #    tracking_logic = PiwikTrackerLogic()
-    #    kwargs = tracking_logic.get_track_kwargs(request)
-    #    try:
-    #        tracking_logic.do_track_page_view(fail_silently=False, **kwargs)
-    #    except urllib_errors as e:
-    #        failed.append(('Piwik API', "API call failed: %s" % (e)))
-    #    else:
-    #        passed.append(('Piwik API', 'okay'))
-
     # Backup server
     #if not settings.BACKUP_SERVER or not settings.BACKUP_USER or not settings.BACKUP_PATH or not settings.BACKUP_PASSPHRASE:
     #    failed.append(('Backup server', 'Backup server settings not all present'))
