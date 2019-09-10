bb1ce2b0df7ea3647c2fe7bf71b21b3c2edb3df0
Phil Boutros <philb@philb.ca>
2019-09-10 08:39:21 -0700

    Add potential supervisors to the CMPT PhD Requirements report

--- reports/reportlib/reports/cmpt_grad_requirements.py
+++ reports/reportlib/reports/cmpt_grad_requirements.py
@@ -26,6 +26,7 @@ class CMPTGradRequirementsReport(Report):
         program = GradProgram.objects.get(label="PhD", unit__label='CMPT')
         all_students = GradStudent.objects.filter(program=program, current_status='ACTI')
         senior_supervisors = Supervisor.objects.filter(student__in=all_students, supervisor_type='SEN', removed=False)
+        potential_supervisors = Supervisor.objects.filter(student__in=all_students, supervisor_type='POT', removed=False)
 
         # Split them up between the 3 tables we want:
         table1_students = all_students.filter(start_semester__lte=current_semester.offset(-2))
@@ -56,7 +57,12 @@ class CMPTGradRequirementsReport(Report):
             if supervisor:
                 return '%s <%s>' % (supervisor.supervisor.name(), supervisor.supervisor.email())
             else:
-                return 'None'
+                supervisor = potential_supervisors.filter(student=student).first()
+                if supervisor:
+                    return '%s (potential) <%s>' % (supervisor.supervisor.name(), supervisor.supervisor.email())
+                else:
+                    return 'None'
+
 
         # Get the lists of students from the master list who have completed the various requirements.
         students1_completed = CompletedRequirement.objects.values_list('student', flat=True)\
