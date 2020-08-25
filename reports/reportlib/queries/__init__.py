0bb557b9aed83fef3e34d47a49143ccc232f3ef2
Greg Baker <ggbaker@sfu.ca>
2020-08-25 12:48:51 -0700

    remove reference to removed file

--- reports/reportlib/queries/__init__.py
+++ reports/reportlib/queries/__init__.py
@@ -1,6 +1,5 @@
 from .student_course_query import *
 from .repeatable_course_query import *
-from .investigative_queries import *
 from .personal_query import * 
 from .program_and_plan import *
 from .gpa_query import *
