6076913bc0effb20a1b3e7f93819805206f912a2
Greg Baker <ggbaker@sfu.ca>
2020-08-26 16:08:15 -0700

    config updates for new deployment style

--- courses/localsettings-proddev-example.py
+++ courses/localsettings-proddev-example.py
@@ -1,10 +1,13 @@
 DEPLOY_MODE = 'proddev'
 
+import os
+MOSS_DISTRIBUTION_PATH = os.path.join(os.environ['HOME'], 'moss')
+DB_BACKUP_DIR = os.path.join(os.environ['COURSYS_DATA_ROOT'], 'db_backup')
+SUBMISSION_PATH = os.path.join(os.environ['COURSYS_DATA_ROOT'], 'submitted_files')
+
+CSRF_TRUSTED_ORIGINS = ['localhost:8443']
+
 #DEBUG_TOOLBAR = True
 #INTERNAL_IPS = ['127.0.0.1']
 #DISABLE_REPORTING_DB = False
-
-import os
-MOSS_DISTRIBUTION_PATH = os.path.join(os.environ['HOME'], 'moss')
-DB_BACKUP_DIR = '/data/db_backup'
-SUBMISSION_PATH = '/data/submitted_files'
+#FORCE_CAS = True
