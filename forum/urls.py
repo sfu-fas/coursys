bd9126deddff742c3a5d54d2b2804e4faa077062
Greg Baker <ggbaker@sfu.ca>
2021-05-24 13:42:49 -0700

    unify forum views for possible dynamic construction later

--- forum/urls.py
+++ forum/urls.py
@@ -9,7 +9,6 @@ COMPONENT_SLUG = '(?P<thread_slug>' + SLUG_RE + ')'
 
 forum_patterns = [ # prefix /COURSE_SLUG/forum/
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<post_number>\d+)?$', views.index, name='view_thread'),
+    url(r'^(?P<post_number>\d+)?$', views.view_thread, name='view_thread'),
     url(r'^new', views.new_thread, name='new_thread'),
-    #url(r'^view/' + COMPONENT_SLUG, views.view_thread, name='view_thread'),
 ]
