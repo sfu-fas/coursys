e0ae1e2385e81d8b7e8f641b6df406b7d0f89095
Greg Baker <ggbaker@sfu.ca>
2021-10-21 13:17:12 -0700

    remove hack around absolute paths

--- pages/views.py
+++ pages/views.py
@@ -190,9 +190,6 @@ def view_page(request, course_slug, page_label):
         if request.path != url:
             return HttpResponseRedirect(url)
 
-    if version.file_attachment and version.file_attachment.name.startswith('/data/submitted_files/'):
-        version.file_attachment.name = version.file_attachment.name.replace('/data/submitted_files', '/filestore/prod/submitted_files')
-
     context = {'offering': offering, 'page': page, 'version': version,
                'can_edit': can_edit, 'is_index': is_index, 'redirect_url': redirect_url}
     return render(request, 'pages/view_page.html', context)
@@ -209,9 +206,6 @@ def _get_file(request, course_slug, page_label, disposition):
     offering = get_object_or_404(CourseOffering, slug=course_slug)
     page = get_object_or_404(Page, offering=offering, label=page_label)
     version = page.current_version()
-
-    if version.file_attachment and version.file_attachment.name.startswith('/data/submitted_files/'):
-        version.file_attachment.name = version.file_attachment.name.replace('/data/submitted_files', '/filestore/prod/submitted_files')
 
     if not version.is_filepage():
         return NotFoundResponse(request)
