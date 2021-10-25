4d88aa95e454d542195cba86ac3b90c4db22e041
reneechong12 <82476895+reneechong12@users.noreply.github.com>
2021-10-25 09:59:36 -0700

    Update __init__.py

--- grad/views/__init__.py
+++ grad/views/__init__.py
@@ -47,6 +47,7 @@ from .save_search import save_search
 from .delete_savedsearch import delete_savedsearch
 from .student_financials import student_financials
 from .financials import financials
+from .financials_report import financials_report
 from .manage_scholarships import manage_scholarships
 from .remove_scholarship import remove_scholarship
 from .manage_scholarshiptypes import new_scholarshiptype, manage_scholarshiptypes, edit_scholarshiptype, \
