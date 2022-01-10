bb5ba1c00815ecbcf217e92f9ab3b4ffa175275d
Greg Baker <ggbaker@sfu.ca>
2022-01-10 09:24:14 -0800

    dependancy security updates

--- courses/localsettings-production-example.py
+++ courses/localsettings-production-example.py
@@ -3,3 +3,7 @@ MORE_ALLOWED_HOSTS = []
 DO_IMPORTING_HERE = True
 MOSS_DISTRIBUTION_PATH = '/home/coursys/moss'
 SIMS_DB_SERVER = 'foo.dc.sfu.ca'
+
+# from django.utils.html import mark_safe
+# SERVER_MESSAGE_INDEX = mark_safe('<p class="infomessage"><i class="fas fa-info-circle"></i> Coursys continues to exist.</p>')
+
