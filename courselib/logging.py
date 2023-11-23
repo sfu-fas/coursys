94803f7c9e3aed6982b137593851773f52fc6e0b
Greg Baker <ggbaker@sfu.ca>
2023-11-22 17:23:50 -0800

    purge couchdb experiment

--- courselib/logging.py
+++ courselib/logging.py
@@ -4,13 +4,6 @@
 from courses import localsettings
 from courses import secrets
 
-RABBITMQ_USER = getattr(localsettings, 'RABBITMQ_USER', 'coursys')
-RABBITMQ_PASSWORD = getattr(secrets, 'RABBITMQ_PASSWORD', 'the_rabbitmq_password')
-RABBITMQ_HOSTPORT = getattr(localsettings, 'RABBITMQ_HOSTPORT', 'localhost:5672')
-RABBITMQ_VHOST = getattr(localsettings, 'RABBITMQ_VHOST', 'myvhost')
-
-RABBITMQ_HOST, RABBITMQ_PORT = RABBITMQ_HOSTPORT.split(':')
-
 POSSIBLE_LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -42,22 +35,10 @@ POSSIBLE_LOGGING_CONFIG = {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler',
         },
-        'rabbitmq': {
-            'level': 'DEBUG',
-            'class': 'python_logging_rabbitmq.RabbitMQHandlerOneWay',
-            'host': RABBITMQ_HOST,
-            'port': int(RABBITMQ_PORT),
-            'username': RABBITMQ_USER,
-            'password': RABBITMQ_PASSWORD,
-            'exchange': 'log',
-            'declare_exchange': True,
-            'connection_params': {'virtual_host': RABBITMQ_VHOST, 'connection_attempts': 4, 'socket_timeout': 5000},
-            'expiration': str(7 * 24 * 3600 * 1000),
-        },
     },
     'loggers': {
         'django-logging': {
-            'handlers': ['mail_admins', 'rabbitmq'],
+            'handlers': ['mail_admins'],
             'level': 'DEBUG',
         }
     }
