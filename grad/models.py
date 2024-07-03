b1c8e63835cf8b79875b194ac0ebefb1a3e4bf00
Skye Mason <skyemason43@gmail.com>
2024-07-03 16:29:48 -0700

    add new grad program fields

--- grad/models.py
+++ grad/models.py
@@ -15,11 +15,18 @@ from django.conf import settings
 import django.db.transaction
 import math
 
+CATEGORY_CHOICES = (
+    ("MA", "Masters"),
+    ("DR", "Doctoral"),
+    ("OT", "Other")
+)
+
 class GradProgram(models.Model):
     unit = models.ForeignKey(Unit, null=False, blank=False, on_delete=models.PROTECT)
     label = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=100, blank=True)
-    
+    grad_category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default="OT")
+    expected_completion_terms = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Last Updated At')
     created_by = models.CharField(max_length=32, null=False, help_text='Grad Program created by.')
