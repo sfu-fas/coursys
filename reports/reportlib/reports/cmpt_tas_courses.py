3e044dfd71da6c6da1d5ca328675bd5b8ea0e521
renee <ylc12@sfu.ca>
2024-09-20 13:05:13 -0700

    CMPT TA courses report

--- reports/reportlib/reports/cmpt_tas_courses.py
+++ reports/reportlib/reports/cmpt_tas_courses.py
@@ -7,7 +7,7 @@ from tacontracts.models import TACourse as tacontracts_courses
 class CMPTTACoursesReport(Report):
     title = "CMPT TAs and Courses for the Current Semester"
     description = "A report of all accepted/signed CMPT TA contracts with courses in the current semester"
-
+    
     def run(self):
         semester = Semester.current()
         units = Unit.objects.filter(label__in=['CMPT'])
@@ -22,17 +22,18 @@ class CMPTTACoursesReport(Report):
                                                          contract__category__hiring_semester__unit__in=units).order_by("course__name")
 
         results = Table()
-        results.append_column('Name')
+        results.append_column('Name of TA')
         results.append_column('SFU ID')
         results.append_column('User ID')
-        results.append_column('Course(s)')
+        results.append_column('Course Number')
+        results.append_column('Course Maillist')
+        results.append_column('Course Name')
+        results.append_column('Instructor UserID(s)')
 
         for ta in tas:
-            results.append_row([ta.contract.application.person.sortname(), ta.contract.application.person.emplid, ta.contract.application.person.userid, ta.course.name()])
+            results.append_row([ta.contract.application.person.name_with_pref(), ta.contract.application.person.emplid, ta.contract.application.person.userid, ta.course.name(), ta.course.maillist(), ta.course.title, "; ".join(p.userid for p in ta.course.instructors())])
 
         for ta in tacontracts:
-            results.append_row([ta.contract.person.sortname(), ta.contract.person.emplid, ta.contract.person.userid, ta.course.name()])
-
-        results = results.flatten('SFU ID')
+            results.append_row([ta.contract.person.name_with_pref(), ta.contract.person.emplid, ta.contract.person.userid, ta.course.name(), ta.course.maillist(), ta.course.title, ta.course.instructors_str()])
 
         self.artifacts.append(results)
