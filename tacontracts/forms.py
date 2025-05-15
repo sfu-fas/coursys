1e30e1941867a5af0bef60fdf59d478fd89af632
Skye Mason <17059664+skyemason@users.noreply.github.com>
2025-05-15 15:29:49 -0700

    ta forms fix (#385)

--- tacontracts/forms.py
+++ tacontracts/forms.py
@@ -66,7 +66,7 @@ class TAContractForm(forms.ModelForm):
         self.fields['category'].queryset = TACategory.objects.visible(hiring_semester)
         # should not be able to edit person after emails have been sent
         if self.instance and self.instance.has_emails():
-            self.fields['person'].disabled = True
+            self.fields['person'].widget.attrs['readonly'] = True
 
 
     person = PersonField()
