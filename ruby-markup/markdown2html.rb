8fc659e8244b95488cbe2feb7604eed0a658ada2
Greg Baker <ggbaker@sfu.ca>
2025-08-25 12:00:51 -0700

    purge Ruby-based github markdown code

--- ruby-markup/markdown2html.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-require 'github/markup'
-
-data = STDIN.read
-# allowing UNSAFE here, because we sanitize with bleach in Python
-html = GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, data, options: {commonmarker_opts: [:UNSAFE]})
-puts(html)
