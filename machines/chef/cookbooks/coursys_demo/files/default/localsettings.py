4562fa6bd9e952be6bfc3a3fc5101e101f5594ce
Greg Baker <ggbaker@sfu.ca>
2025-08-27 15:51:36 -0700

    purge old unreleased modules and VM setup (now in deploy/)

--- machines/chef/cookbooks/coursys_demo/files/default/localsettings.py
+++
@@ -1,6 +0,0 @@
-DEPLOY_MODE = "proddev"
-COMPRESS_ENABLED = True
-USE_CELERY = True
-DISABLE_REPORTING_DB = True
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.BaseSignalProcessor'
-BASE_ABS_URL = "https://coursys-demo.cs.sfu.ca"
