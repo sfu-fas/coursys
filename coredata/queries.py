a879da6221745913cae0d5c41529baa4f9fdb392
Greg Baker <ggbaker@sfu.ca>
2025-09-11 14:38:44 -0700

    simplify import: don't handle made-up cases

--- coredata/queries.py
+++ coredata/queries.py
@@ -1660,10 +1660,7 @@ def import_person(p, commit=True, grad_data=False):
     p.title = title
     p.config['lastimport'] = int(time.time())
 
-    if not pref_first_name and legal_first_name:
-        # if there is no preferred first name in SIMS, accept the legal name as the only option: seems to not occur in practice
-        p.first_name = legal_first_name
-    elif legal_first_name != pref_first_name:
+    if legal_first_name != pref_first_name:
         # where legal name is distinct, store it just in case a lawyer tells us it must be used somewhere
         p.config['legal_first_name_do_not_use'] = legal_first_name
     elif 'legal_first_name_do_not_use' in p.config:
