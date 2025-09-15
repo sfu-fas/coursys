8c3e18f51367d748e770e632127bd07ada3e6998
Greg Baker <ggbaker@sfu.ca>
2025-09-15 09:58:49 -0700

    revert to legal first name in grad letters, temporarily

--- grad/models.py
+++ grad/models.py
@@ -828,7 +828,8 @@ class GradStudent(models.Model, ConditionalSaveMixin):
                 'He_She' : heshe.title(),
                 'him_her' : himher,
                 'Him_Her' : himher.title(),
-                'first_name': self.person.first_name,
+                # temporarily use legal first name here: this represents the past behaviour and is pending better logic for users to select which name should be used in this case
+                'first_name': self.person.config.get('legal_first_name_do_not_use') or self.person.first_name,
                 'last_name': self.person.last_name,
                 'emplid': emplid, 
                 'promise': promise,
