fbd3198fda7de0a7408761293f846803bf520246
Greg Baker <ggbaker@sfu.ca>
2026-07-01 11:10:00 -0700

    refactor celery beat schedule, specifying queues where appropriate

--- courses/celery_schedule.py
+++ courses/celery_schedule.py
@@ -1,89 +1,40 @@
 from celery.schedules import crontab
 
-CELERY_BEAT_SCHEDULE = {
-    'coredata.tasks.beat_test': {
-        'task': 'coredata.tasks.beat_test',
-        'schedule': crontab(minute='*/5', hour='*'),
-    },
-    'coredata.tasks.regular_backup': {
-        'task': 'coredata.tasks.regular_backup',
-        'schedule': crontab(minute=0, hour='*/3'),
-    },
-    'coredata.tasks.daily_import': {
-        'task': 'coredata.tasks.daily_import',
-        'schedule': crontab(minute='30', hour='8'),
-    },
-    'grad.tasks.grad_daily_import': {
-        'task': 'grad.tasks.grad_daily_import',
-        'schedule': crontab(minute='40', hour='8'),
-    },
-    'coredata.tasks.check_sims_connection': {
-        'task': 'coredata.tasks.check_sims_connection',
-        'schedule': crontab(minute=0, hour='*/3'),
-    },
-    'coredata.tasks.expire_sessions_conveniently': {
-        'task': 'coredata.tasks.expire_sessions_conveniently',
-        'schedule': crontab(minute='0', hour='4'),
-    },
-    'coredata.tasks.haystack_rebuild': {
-        'task': 'coredata.tasks.haystack_rebuild',
-        'schedule': crontab(minute='0', hour='2', day_of_week='saturday'),
-    },
-    'coredata.tasks.expiring_roles': {
-        'task': 'coredata.tasks.expiring_roles',
-        'schedule': crontab(minute='30', hour='7', day_of_week='mon,thu'),
-    },
-    'dashboard.tasks.photo_password_update_task': {
-        'task': 'dashboard.tasks.photo_password_update_task',
-        'schedule': crontab(day_of_month="10,20,30", hour=2, minute=0),
-    },
 
-    'advisornotes.tasks.cleanup_advising_surveys': {
-        'task': 'advisornotes.tasks.cleanup_advising_surveys',
-        'schedule': crontab(minute=0, hour='2'),
-    },
-    'advisornotes.tasks.program_info_for_advisorvisits': {
-        'task': 'advisornotes.tasks.program_info_for_advisorvisits',
-        'schedule': crontab(minute=0, hour='2'),
-    },
-    'forum.tasks.send_digests': {
-        'task': 'forum.tasks.send_digests',
-        'schedule': crontab(hour='*', minute='0'),
-    },
-    'grad.tasks.update_statuses_to_current': {
-        'task': 'grad.tasks.update_statuses_to_current',
-        'schedule': crontab(minute=0, hour=3),
-    },
-    'onlineforms.tasks.waiting_forms_reminder': {
-        'task': 'onlineforms.tasks.waiting_forms_reminder',
-        'schedule': crontab(day_of_week="1", hour="13", minute="0"),
-    },
-    'onlineforms.tasks.reject_dormant_initial': {
-        'task': 'onlineforms.tasks.reject_dormant_initial',
-        'schedule': crontab(hour="18", minute="0"),
-    },
-    'ra.tasks.expiring_ras_reminder': {
-        'task': 'ra.tasks.expiring_ras_reminder',
-        'schedule': crontab(minute='0', hour='13'),
-    },
-    'reminders.tasks.daily_reminders': {
-        'task': 'reminders.tasks.daily_reminders',
-        'schedule': crontab(minute='0', hour='9'),
-    },
-    'reports.tasks.run_regular_reports': {
-        'task': 'reports.tasks.run_regular_reports',
-        'schedule': crontab(hour=9, minute=15),
-    },
-    'ta.tasks.check_and_execute_reminders': {
-        'task': 'ta.tasks.check_and_execute_reminders',
-        'schedule': crontab(minute='00', hour='8'),
-    },    
-    'log.tasks.log_regular': {
-        'task': 'log.tasks.log_regular',
-        'schedule': crontab(hour='*', minute='0,15,30,45')
-    },
-    'log.tasks.log_avg_request_duration': {
-        'task': 'log.tasks.log_avg_request_duration',
-        'schedule': crontab(hour='*', minute=0)
-    },
-}
+def beat(task: str, schedule: int | crontab, *, queue: str = 'batch'):
+    """
+    Helper to create a key/value pair for the celerybeat schedule.
+    """
+    entry = {
+        'task': task,
+        'schedule': schedule,
+        'queue': queue,
+        'options': { 'queue': queue, 'routing_key': queue },
+    }
+    return (task, entry)
+
+
+CELERY_BEAT_SCHEDULE = dict([
+    beat('coredata.tasks.beat_test', 300, queue='fast'),
+    beat('coredata.tasks.regular_backup', crontab(minute=0, hour='*/3'), queue='batch'),
+    beat('coredata.tasks.csrpt_refresh_periodic', 7200, queue='sims'),
+    beat('coredata.tasks.daily_import', crontab(minute='30', hour='8'), queue='sims'),
+    beat('grad.tasks.grad_daily_import', crontab(minute='40', hour='8'), queue='sims'),
+    beat('coredata.tasks.check_sims_connection', crontab(minute=0, hour='*/3'), queue='sims'),
+    beat('coredata.tasks.expire_sessions_conveniently', crontab(minute='0', hour='4')),
+    beat('coredata.tasks.haystack_rebuild', crontab(minute='0', hour='2', day_of_week='saturday')),
+    beat('coredata.tasks.expiring_roles', crontab(minute='30', hour='7', day_of_week='mon,thu')),
+    beat('dashboard.tasks.photo_password_update_task', crontab(day_of_month="10,20,30", hour=2, minute=0)),
+    beat('advisornotes.tasks.cleanup_advising_surveys', crontab(minute=0, hour='2')),
+    beat('advisornotes.tasks.program_info_for_advisorvisits', crontab(minute=5, hour='2')),
+    beat('forum.tasks.send_digests', crontab(hour='*', minute='0')),
+    beat('grad.tasks.update_statuses_to_current', crontab(minute=0, hour=3)),
+    beat('onlineforms.tasks.waiting_forms_reminder', crontab(day_of_week="1", hour="13", minute="0")),
+    beat('onlineforms.tasks.reject_dormant_initial', crontab(hour="18", minute="0")),
+    beat('ra.tasks.expiring_ras_reminder', crontab(minute='0', hour='13')),
+    beat('reminders.tasks.daily_reminders', crontab(minute='0', hour='9')),
+    beat('reports.tasks.run_regular_reports', crontab(hour=9, minute=15)),
+    beat('ta.tasks.check_and_execute_reminders', crontab(minute='0', hour='8')),
+    beat('log.tasks.log_regular', crontab(hour='*', minute='0,15,30,45'), queue='fast'),
+    beat('log.tasks.log_avg_request_duration', crontab(hour='*', minute=0), queue='fast'),
+])
