6de89f3c45eebd0544ce86d0d4a5bbc7bceee551
Greg Baker <ggbaker@sfu.ca>
2026-07-14 12:07:36 -0700

    Let tests pass in proddev deploy mode, with elasticsearch as search backend (#459)
    * make sure periodic tasks obey DO_IMPORTING_HERE if they have external effects
    
    * fix ta/tasks.py
    
    * add some convenient management commands
    
    * move CSRPT auth into the web frontend, away from a manual shell command
    
    * let tests run with elasticsearch as haystack backend

--- courses/settings.py
+++ courses/settings.py
@@ -134,7 +134,10 @@ FIXTURE_DIRS = [os.path.join(BASE_DIR, 'fixtures')]
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Disable migrations only when running tests.
-if 'test' in sys.argv[1:]:
+IN_TESTING = 'test' in sys.argv[1:]
+if IN_TESTING:
+    if DEPLOY_MODE == 'production':
+        raise ValueError
     MIGRATION_MODULES = {}
     for m in INSTALLED_APPS:
         MIGRATION_MODULES[m] = None
@@ -250,6 +253,8 @@ if DEPLOY_MODE in ['production', 'proddev']:
             'TIMEOUT': 60,
         },
     }
+    if IN_TESTING:
+        HAYSTACK_CONNECTIONS['default']['INDEX_NAME'] = 'haystack-testing'
     DB_BACKUP_DIR = getattr(localsettings, 'DB_BACKUP_DIR', os.path.join(os.environ.get('COURSYS_DATA_ROOT', '.'), 'db_backup'))
 
 else:
