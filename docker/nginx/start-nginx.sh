759b4a3c3760d7a8a0dfdadbbc9c722421425cb2
Greg Baker <ggbaker@sfu.ca>
2026-08-26 07:41:48 -0700

    fix nginx config file bootstrap

--- docker/nginx/start-nginx.sh
+++ docker/nginx/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Ensure *some* nginx-backends.conf exists in a sane state 
-[ -f /dynamic_config/nginx-backends.conf ] || cp /etc/nginx/backend-configs/backends-default.conf /dynamic_config/nginx-backends.conf
+[ -f /dynamic_config/nginx-backends.conf ] || cp /etc/nginx/backends-default.conf /dynamic_config/nginx-backends.conf
 chown ${UID} /dynamic_config/nginx-backends.conf
 chmod 0644 /dynamic_config/nginx-backends.conf
 
